d9da7f46889b65e6d06d6b19161b4a415a7f5617
Matthew Wilding <mbwilding@gmail.com>
2023-09-30 18:46:35 +0800

    Changed actions preview hotkey

--- after/plugin/actions-preview.lua
+++ after/plugin/actions-preview.lua
@@ -2,4 +2,4 @@ require('actions-preview').setup {
 
 }
 
-vim.keymap.set({ 'n', 'v' }, '<leader>la', require('actions-preview').code_actions)
+vim.keymap.set({ 'n', 'v' }, '<leader>ap', require('actions-preview').code_actions)
