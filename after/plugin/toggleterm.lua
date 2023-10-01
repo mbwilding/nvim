0219a7c7cf66ab61d91ea9e3b9c57164d6f87c38
Matt Wilding <tabmaw@rwwa.com.au>
2023-10-01 17:53:01 +0800

    Platform specific code

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,11 +1,18 @@
 local toggle_term = require("toggleterm")
 
+local shell_command
+if IsWindows then
+    shell_command = "pwsh.exe -NoLogo"
+else
+    shell_command = nil
+end
+
 toggle_term.setup {
     -- open_mapping = [[<c-\>]],
     start_in_insert = true,
     terminal_mappings = true,
     -- direction = "float",
-    -- shell = "pwsh.exe -NoLogo", -- Windows only
+    shell = shell_command,
     auto_scroll = true,
     -- persist_mode = true,
     persist_size = true,
