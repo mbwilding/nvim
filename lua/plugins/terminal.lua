8197bf7204c6bdb2a6df4c7c873af52fb7f81849
Matthew Wilding <mbwilding@gmail.com>
2024-02-16 17:22:19 +0800

    Update

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -20,6 +20,13 @@
             -- persist_mode = true,
             persist_size = true,
             close_on_exit = true,
+            hide_numbers = true,
+            shade_terminals = true,
         }
+
+        vim.keymap.set({ "n", "t" }, "<leader>th", "<cmd>:1ToggleTerm direction=horizontal size=20<CR>")
+        vim.keymap.set({ "n", "t" }, "<leader>tt", "<cmd>:2ToggleTerm direction=horizontal size=20<CR>")
+        vim.keymap.set({ "n", "t" }, "<leader>tn", "<cmd>:3ToggleTerm direction=horizontal size=20<CR>")
+        vim.keymap.set({ "n", "t" }, "<leader>ts", "<cmd>:4ToggleTerm direction=float<CR>")
     end,
 }
