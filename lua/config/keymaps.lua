fa903b18a8fffff161763c692067a6c7b682eaff
Matthew Wilding <mbwilding@gmail.com>
2024-02-19 16:04:53 +0800

    Update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
+-- Map ; to :
+vim.keymap.set('n', ';', ':', { noremap = true, silent = true })
+
 -- Disables search highlight
 vim.keymap.set('n', '<CR>', ':noh<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<ESC>', ':noh<ESC>', { noremap = true, silent = true })
