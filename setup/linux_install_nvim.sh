0c1ff0af6818b036662694f044344274f0967d25
Matt Wilding <tabmaw@rwwa.com.au>
2024-02-22 15:17:07 +0800

    Update

--- setup/linux_install_nvim.sh
+++ setup/linux_install_nvim.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+nightly = 1
+
 # Install Neovim
-API_URL="https://api.github.com/repos/neovim/neovim/releases/tags/nightly"
+if [ "$nightly" -eq 1 ]; then
+    API_URL="https://api.github.com/repos/neovim/neovim/releases/tags/nightly"
+else
+    API_URL="https://api.github.com/repos/neovim/neovim/releases/latest"
+fi
+
 APPIMAGE_URL=$(wget -qO- "$API_URL" | jq -r '.assets[] | select(.name | endswith("nvim.appimage")) | .browser_download_url')
 wget --quiet "$APPIMAGE_URL" --output-document nvim
 chmod +x nvim
