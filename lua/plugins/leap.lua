7cc668064fbcc155bdfaa7fb7a0f35d5fdb51868
Matthew Wilding <mbwilding@gmail.com>
2024-02-24 01:14:09 +0800

    Leap added, s/S to search

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,14 @@
+return {
+    "ggandor/leap.nvim",
+    dependencies = {
+        "tpope/vim-repeat"
+    },
+    config = function()
+        local leap = require("leap")
+        leap.add_default_mappings()
+        leap.opts.special_keys.prev_target = '<bs>'
+        leap.opts.special_keys.prev_group = '<bs>'
+        leap.opts.labels = "aoeuidhtnspyfgcrlqjkxbmwvz"
+        require('leap.user').set_repeat_keys('<cr>', '<bs>')
+    end
+}
