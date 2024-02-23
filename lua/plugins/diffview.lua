f2cabdf0f1ebd39322171bd6d00e9de777d625cc
Matthew Wilding <mbwilding@gmail.com>
2024-02-23 17:12:04 +0800

    Update

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+    "sindrets/diffview.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+        vim.keymap.set("n", "<leader>gdh", "<cmd>DiffviewFileHistory %<CR>", { desc = "Diffview: Current File History" })
+        vim.keymap.set("n", "<leader>gdH", "<cmd>DiffviewFileHistory<CR>", { desc = "Diffview: Repo File History" })
+        vim.keymap.set("n", "<leader>gdf", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle File Panel" })
+        vim.keymap.set("n", "<leader>gdF", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview: Focus File Panel" })
+        vim.keymap.set("n", "<leader>gdr", "<cmd>DiffviewRefresh<CR>", { desc = "Diffview: Refresh" })
+    end
+}
