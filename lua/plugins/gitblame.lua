49d937c51c9b788f0fcb1989d10e4f5234205a57
Matthew Wilding <mbwilding@gmail.com>
2024-02-23 17:40:44 +0800

    Update

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -4,8 +4,6 @@ return {
         vim.g.gitblame_enabled = false
         vim.g.gitblame_delay = 0
 
-        vim.api.nvim_set_keymap('n', '<leader>gbb', '<cmd>GitBlameToggle<CR>', { noremap = true, silent = true })
-
         -- Lua function to set gitblame delay
         local function set_gitblame_delay(delay)
             vim.g.gitblame_delay = delay
@@ -25,7 +23,7 @@ return {
             { nargs = 1 }
         )
 
-        -- Key binding to invoke the command
-        vim.api.nvim_set_keymap('n', '<leader>gbd', ':SetGitBlameDelay ', { noremap = true, silent = false })
+        vim.api.nvim_set_keymap('n', '<leader>gbb', '<cmd>GitBlameToggle<CR>', { noremap = true, silent = true, desc = "Gitblame: Toggle" })
+        vim.api.nvim_set_keymap('n', '<leader>gbd', ':SetGitBlameDelay ', { noremap = true, silent = false, desc = "Gitblame: View Delay" })
     end
 }
