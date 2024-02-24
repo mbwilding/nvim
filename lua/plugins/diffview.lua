675ed5d0cc99904c70aa64e90cd20fbaa8992f79
Matthew Wilding <mbwilding@gmail.com>
2024-02-24 17:20:55 +0800

    Update

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -4,11 +4,11 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     config = function()
-        vim.keymap.set("n", "<leader>gdm", "<cmd>DiffviewOpen<CR>", { desc = "Diffview: Open Merge/Rebase" })
-        vim.keymap.set("n", "<leader>gdh", "<cmd>DiffviewFileHistory %<CR>", { desc = "Diffview: Current File History" })
-        vim.keymap.set("n", "<leader>gdH", "<cmd>DiffviewFileHistory<CR>", { desc = "Diffview: Repo File History" })
-        vim.keymap.set("n", "<leader>gdf", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle File Panel" })
-        vim.keymap.set("n", "<leader>gdF", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview: Focus File Panel" })
-        vim.keymap.set("n", "<leader>gdr", "<cmd>DiffviewRefresh<CR>", { desc = "Diffview: Refresh" })
+        vim.keymap.set("n", "<leader>dvm", "<cmd>DiffviewOpen<CR>", { desc = "Diffview: Open Merge/Rebase" })
+        vim.keymap.set("n", "<leader>dvh", "<cmd>DiffviewFileHistory %<CR>", { desc = "Diffview: Current File History" })
+        vim.keymap.set("n", "<leader>dvH", "<cmd>DiffviewFileHistory<CR>", { desc = "Diffview: Repo File History" })
+        vim.keymap.set("n", "<leader>dvf", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle File Panel" })
+        vim.keymap.set("n", "<leader>dvF", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview: Focus File Panel" })
+        vim.keymap.set("n", "<leader>dvr", "<cmd>DiffviewRefresh<CR>", { desc = "Diffview: Refresh" })
     end
 }
