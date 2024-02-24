7696e3748a074ce72f19cf8b1098a98465d4023a
Matthew Wilding <mbwilding@gmail.com>
2024-02-24 22:50:49 +0800

    Update

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,5 +1,6 @@
 return {
-    "andweeb/presence.nvim",
+    "jiriks74/presence.nvim",
+    event = "UIEnter",
     opts = {
         -- General options
         auto_update         = true,                 -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
