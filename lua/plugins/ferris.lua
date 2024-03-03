04104dd23df2f52404704d69398c361fa5fa80be
Matthew Wilding <mbwilding@gmail.com>
2024-03-04 00:20:58 +0800

    Update

--- lua/plugins/ferris.lua
+++ lua/plugins/ferris.lua
@@ -3,27 +3,27 @@ return {
     config = function()
         require("ferris").setup()
 
-        vim.keymap.set("n", "<leader>fe", require("ferris.methods.expand_macro"),
+        vim.keymap.set("n", "<leader>re", require("ferris.methods.expand_macro"),
             { silent = true, desc = "Ferris: Expand macro" })
-        vim.keymap.set("n", "<leader>fj", require("ferris.methods.join_lines"),
+        vim.keymap.set("n", "<leader>rj", require("ferris.methods.join_lines"),
             { silent = true, desc = "Ferris: Join lines" })
-        vim.keymap.set("n", "<leader>fH", require("ferris.methods.view_hir"), { silent = true, desc = "Ferris: HIR" })
-        vim.keymap.set("n", "<leader>fM", require("ferris.methods.view_mir"), { silent = true, desc = "Ferris: MIR" })
-        vim.keymap.set("n", "<leader>fl", require("ferris.methods.view_memory_layout"),
+        vim.keymap.set("n", "<leader>rH", require("ferris.methods.view_hir"), { silent = true, desc = "Ferris: HIR" })
+        vim.keymap.set("n", "<leader>rM", require("ferris.methods.view_mir"), { silent = true, desc = "Ferris: MIR" })
+        vim.keymap.set("n", "<leader>rl", require("ferris.methods.view_memory_layout"),
             { silent = true, desc = "Ferris: Memory layout" })
-        vim.keymap.set("n", "<leader>fi", require("ferris.methods.view_item_tree"),
+        vim.keymap.set("n", "<leader>ri", require("ferris.methods.view_item_tree"),
             { silent = true, desc = "Ferris: Item tree" })
-        vim.keymap.set("n", "<leader>fs", require("ferris.methods.view_syntax_tree"),
+        vim.keymap.set("n", "<leader>rs", require("ferris.methods.view_syntax_tree"),
             { silent = true, desc = "Ferris: Syntax tree" })
-        vim.keymap.set("n", "<leader>fs", require("ferris.methods.open_cargo_toml"),
+        vim.keymap.set("n", "<leader>rc", require("ferris.methods.open_cargo_toml"),
             { silent = true, desc = "Ferris: Open Cargo.toml" })
-        vim.keymap.set("n", "<leader>fp", require("ferris.methods.open_parent_module"),
+        vim.keymap.set("n", "<leader>rp", require("ferris.methods.open_parent_module"),
             { silent = true, desc = "Ferris: Open parent module" })
-        vim.keymap.set("n", "<leader>fd", require("ferris.methods.open_documentation"),
+        vim.keymap.set("n", "<leader>rof", require("ferris.methods.open_documentation"),
             { silent = true, desc = "Ferris: Open documentation" })
-        vim.keymap.set("n", "<leader>fr", require("ferris.methods.reload_workspace"),
+        vim.keymap.set("n", "<leader>rw", require("ferris.methods.reload_workspace"),
             { silent = true, desc = "Ferris: Reload workspace" })
-        vim.keymap.set("n", "<leader>fm", require("ferris.methods.rebuild_macros"),
+        vim.keymap.set("n", "<leader>rm", require("ferris.methods.rebuild_macros"),
             { silent = true, desc = "Ferris: Rebuild macros" })
     end
 }
