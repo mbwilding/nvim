db4e0d303858540e771b38e2d7a54468bd1f7795
Matthew Wilding <mbwilding@gmail.com>
2024-03-13 22:00:03 +0800

    Update

---
+++ lua/plugins/multicursors.lua
@@ -0,0 +1,17 @@
+return {
+	"smoka7/multicursors.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"smoka7/hydra.nvim",
+	},
+	opts = {},
+	cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+	keys = {
+		{
+			mode = { "v", "n" },
+			"<leader>M",
+			"<cmd>MCstart<CR>",
+			desc = "Create a selection for selected text or word under the cursor",
+		},
+	},
+}
