6e0fde7693534980b481f91fb50581e18e566af8
Matthew Wilding <mbwilding@gmail.com>
2024-03-13 21:46:58 +0800

    Update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,27 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    build = ":Copilot auth",
-    module = "copilot",
-    opts = {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        filetypes = {
-            yaml = false,
-            markdown = false,
-            help = false,
-            gitcommit = false,
-            gitrebase = false,
-            hgcommit = false,
-            svn = false,
-            cvs = false,
-        },
-    },
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	build = ":Copilot auth",
+	module = "copilot",
+	opts = {
+		suggestion = { enabled = false },
+		panel = { enabled = false },
+		filetypes = {
+			yaml = false,
+			markdown = false,
+			help = false,
+			gitcommit = false,
+			gitrebase = false,
+			hgcommit = false,
+			svn = false,
+			cvs = false,
+		},
+	},
+	dependencies = {
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
