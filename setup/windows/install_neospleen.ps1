3243580dd8cabe527c513b8b97967e816406dffc
Matthew Wilding <mbwilding@gmail.com>
2024-03-28 22:08:13 +0800

    Windows scripts

---
+++ setup/windows/install_neospleen.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = "Stop"
+
+$fontPath = "$env:USERPROFILE\AppData\Local\Microsoft\Windows\Fonts"
+New-Item -ItemType Directory -Force -Path $fontPath
+Push-Location -Path "$env:USERPROFILE\AppData\Local\Microsoft\Windows\Fonts"
+Remove-Item -Path "NeoSpleen*.ttf" -Force
+Invoke-WebRequest -Uri "https://github.com/mbwilding/NeoSpleen/releases/latest/download/NeoSpleen-NerdFont.ttf" -OutFile "NeoSpleen-NerdFont.ttf"
+Pop-Location
