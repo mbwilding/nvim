b43bb0ba7c8e813ac57639f310b59e8d790ceb06
Matthew Wilding <mbwilding@gmail.com>
2024-04-03 00:38:37 +0800

    Update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,38 +2,51 @@ return {
 	"stevearc/conform.nvim",
 	config = function()
 		require("conform").setup({
-			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			-- Map of filetype to formatters
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
+				-- Conform will run multiple formatters sequentially
+				go = { "goimports", "gofmt" },
+				-- Use a sub-list to run only the first available formatter
 				javascript = { { "prettierd", "prettier" } },
+				-- You can use a function here to determine the formatters dynamically
+				python = function(bufnr)
+					if require("conform").get_formatter_info("ruff_format", bufnr).available then
+						return { "ruff_format" }
+					else
+						return { "isort", "black" }
+					end
+				end,
+				-- Use the "*" filetype to run formatters on all filetypes.
+				["*"] = { "codespell" },
+				-- Use the "_" filetype to run formatters on filetypes that don't
+				-- have other formatters configured.
+				["_"] = { "trim_whitespace" },
 			},
+			-- If this is set, Conform will run the formatter on save.
+			-- It will pass the table to conform.format().
+			-- This can also be a function that returns the table.
+			format_on_save = {
+				-- I recommend these options. See :help conform.format for details.
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
+			-- If this is set, Conform will run the formatter asynchronously after save.
+			-- It will pass the table to conform.format().
+			-- This can also be a function that returns the table.
+			format_after_save = {
+				lsp_fallback = true,
+			},
+			-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+			log_level = vim.log.levels.ERROR,
+			-- Conform will notify you when a formatter errors
+			notify_on_error = true,
+			-- Custom formatters and changes to built-in formatters
 		})
-
-		-- Format on save [async]
-		--vim.api.nvim_create_autocmd("BufWritePre", {
-		--	pattern = "*",
-		--	callback = function(args)
-		--		vim.defer_fn(function()
-		--			require("conform").format({ bufnr = args.buf })
-		--		end, 0) -- Defer the execution, making it asynchronous
-		--	end,
-		--})
-
-		-- Format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				require("conform").format({ bufnr = args.buf })
-			end,
-		})
+		-- You can set formatters_by_ft and formatters directly
+		require("conform").formatters_by_ft.lua = { "stylua" }
+		require("conform").formatters.my_formatter = {
+			command = "my_cmd",
+		}
 	end,
 }
