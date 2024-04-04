f80f545afd6bd3b95686759b1ea27f23aeeebc5b
Matthew Wilding <mbwilding@gmail.com>
2024-04-04 10:22:21 +0800

    Update

--- lua/plugins/gronk.lua
+++ lua/plugins/gronk.lua
@@ -2,5 +2,9 @@ return {
 	"mbwilding/gronk.nvim",
 	config = function()
 		vim.cmd([[colorscheme gronk]])
+
+		vim.keymap.set("n", "<leader>tG", function()
+			vim.cmd([[colorscheme gronk]])
+		end, { desc = "Theme: Gronk" })
 	end,
 }
