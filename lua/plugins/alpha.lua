004fd79867cb867b1b1ad0d076bc11c834eec308
Matthew Wilding <mbwilding@gmail.com>
2024-06-08 15:16:00 +0800

    Update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -5,45 +5,16 @@ return {
 		local alpha = require 'alpha'
 		local startify = require 'alpha.themes.startify'
 
-		startify.section.header.opts = { position = "center" }
 		startify.section.header.val = {
-			"█████   ███   █████  ███  ████      █████  ███                      ",
-			"░░███   ░███  ░░███  ░░░  ░░███     ░░███  ░░░                      ",
-			" ░███   ░███   ░███  ████  ░███   ███████  ████  ████████    ███████",
-			" ░███   ░███   ░███ ░░███  ░███  ███░░███ ░░███ ░░███░░███  ███░░███",
-			" ░░███  █████  ███   ░███  ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███",
-			"  ░░░█████░█████░    ░███  ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███",
-			"    ░░███ ░░███      █████ █████░░████████ █████ ████ █████░░███████",
-			"     ░░░   ░░░      ░░░░░ ░░░░░  ░░░░░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░███",
-			"                                                            ███ ░███",
-			"                                                           ░░██████ ",
-			"                                                            ░░░░░░  "
+			"                                                   ",
+			"██╗    ██╗██╗██╗     ██████╗ ██╗███╗   ██╗ ██████╗ ",
+			"██║    ██║██║██║     ██╔══██╗██║████╗  ██║██╔════╝ ",
+			"██║ █╗ ██║██║██║     ██║  ██║██║██╔██╗ ██║██║  ███╗",
+			"██║███╗██║██║██║     ██║  ██║██║██║╚██╗██║██║   ██║",
+			"╚███╔███╔╝██║███████╗██████╔╝██║██║ ╚████║╚██████╔╝",
+			" ╚══╝╚══╝ ╚═╝╚══════╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ",
 		}
-		-- startify.section.top_buttons.val = {
-		-- 	startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-		-- }
-		-- disable MRU
-		-- startify.section.mru.val = { { type = "padding", val = 0 } }
-		-- disable MRU cwd
-		-- startify.section.mru_cwd.val = { { type = "padding", val = 0 } }
-		-- disable nvim_web_devicons
-		startify.nvim_web_devicons.enabled = true
-		-- startify.nvim_web_devicons.highlight = false
-		-- startify.nvim_web_devicons.highlight = 'Keyword'
-		--
-		-- startify.section.bottom_buttons.val = {
-		-- 	startify.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
-		-- }
-		-- startify.section.footer.val = {
-		-- 	{ type = "text", val = "footer" },
-		-- }
-		-- ignore filetypes in MRU
 
-		-- startify.mru_opts.ignore = function(path, ext)
-		-- 	return
-		-- 		(string.find(path, "COMMIT_EDITMSG"))
-		-- 		or (vim.tbl_contains(default_mru_ignore, ext))
-		-- end
 		alpha.setup(startify.config)
 	end
 }
