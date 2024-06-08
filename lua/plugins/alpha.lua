ce6fce8ccbd72ef776ae19d1704106553140ce22
Matthew Wilding <mbwilding@gmail.com>
2024-06-08 14:13:59 +0800

    Update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,7 +1,49 @@
 return {
 	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	requires = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
-		require("alpha").setup(require("alpha.themes.startify").config)
-	end,
+		local alpha = require 'alpha'
+		local startify = require 'alpha.themes.startify'
+
+		startify.section.header.opts = { position = "center" }
+		startify.section.header.val = {
+			"█████   ███   █████  ███  ████      █████  ███                      ",
+			"░░███   ░███  ░░███  ░░░  ░░███     ░░███  ░░░                      ",
+			" ░███   ░███   ░███  ████  ░███   ███████  ████  ████████    ███████",
+			" ░███   ░███   ░███ ░░███  ░███  ███░░███ ░░███ ░░███░░███  ███░░███",
+			" ░░███  █████  ███   ░███  ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███",
+			"  ░░░█████░█████░    ░███  ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███",
+			"    ░░███ ░░███      █████ █████░░████████ █████ ████ █████░░███████",
+			"     ░░░   ░░░      ░░░░░ ░░░░░  ░░░░░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░███",
+			"                                                            ███ ░███",
+			"                                                           ░░██████ ",
+			"                                                            ░░░░░░  "
+		}
+		-- startify.section.top_buttons.val = {
+		-- 	startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+		-- }
+		-- disable MRU
+		-- startify.section.mru.val = { { type = "padding", val = 0 } }
+		-- disable MRU cwd
+		-- startify.section.mru_cwd.val = { { type = "padding", val = 0 } }
+		-- disable nvim_web_devicons
+		startify.nvim_web_devicons.enabled = true
+		-- startify.nvim_web_devicons.highlight = false
+		-- startify.nvim_web_devicons.highlight = 'Keyword'
+		--
+		-- startify.section.bottom_buttons.val = {
+		-- 	startify.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+		-- }
+		-- startify.section.footer.val = {
+		-- 	{ type = "text", val = "footer" },
+		-- }
+		-- ignore filetypes in MRU
+
+		-- startify.mru_opts.ignore = function(path, ext)
+		-- 	return
+		-- 		(string.find(path, "COMMIT_EDITMSG"))
+		-- 		or (vim.tbl_contains(default_mru_ignore, ext))
+		-- end
+		alpha.setup(startify.config)
+	end
 }
