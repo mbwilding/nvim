11beef4eb6a70818b76b4049dcf9b7559dfca399
Matthew Wilding <mbwilding@gmail.com>
2024-06-13 23:58:16 +0800

    Update

--- lua/plugins/profiles.lua
+++ lua/plugins/profiles.lua
@@ -8,11 +8,11 @@ return {
 	config = function()
 		local profiles = require("profiles")
 
-		vim.keymap.set("n", "<leader>rp", function()
+		vim.keymap.set("n", "<leader>o", function()
 			profiles.select_profile()
 		end, { desc = "Profiles: Select Local" })
 
-		vim.keymap.set("n", "<leader>ry", function()
+		vim.keymap.set("n", "<leader>O", function()
 			profiles.select_default_profile()
 		end, { desc = "Profiles: Select Default" })
 	end,
