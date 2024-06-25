05c185396e4a967147fe3eb19c6cafb1acea16e4
Matthew Wilding <mbwilding@gmail.com>
2024-06-25 09:10:43 +0800

    Update

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,19 +1,27 @@
 return {
-	"sindrets/diffview.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>dvm", "<cmd>DiffviewOpen<CR>", { desc = "Diffview: Open Merge/Rebase" })
-		vim.keymap.set(
-			"n",
-			"<leader>dvh",
-			"<cmd>DiffviewFileHistory %<CR>",
-			{ desc = "Diffview: Current File History" }
-		)
-		vim.keymap.set("n", "<leader>dvH", "<cmd>DiffviewFileHistory<CR>", { desc = "Diffview: Repo File History" })
-		vim.keymap.set("n", "<leader>dvf", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle File Panel" })
-		vim.keymap.set("n", "<leader>dvF", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview: Focus File Panel" })
-		vim.keymap.set("n", "<leader>dvr", "<cmd>DiffviewRefresh<CR>", { desc = "Diffview: Refresh" })
-	end,
+    "sindrets/diffview.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("diffview").setup({
+            hooks = {
+                diff_buf_win_enter = function()
+                    vim.opt_local.foldenable = false
+                end,
+            },
+        })
+
+        vim.keymap.set("n", "<leader>dvm", "<cmd>DiffviewOpen<CR>", { desc = "Diffview: Open Merge/Rebase" })
+        vim.keymap.set(
+            "n",
+            "<leader>dvh",
+            "<cmd>DiffviewFileHistory %<CR>",
+            { desc = "Diffview: Current File History" }
+        )
+        vim.keymap.set("n", "<leader>dvH", "<cmd>DiffviewFileHistory<CR>", { desc = "Diffview: Repo File History" })
+        vim.keymap.set("n", "<leader>dvf", "<cmd>DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle File Panel" })
+        vim.keymap.set("n", "<leader>dvF", "<cmd>DiffviewFocusFiles<CR>", { desc = "Diffview: Focus File Panel" })
+        vim.keymap.set("n", "<leader>dvr", "<cmd>DiffviewRefresh<CR>", { desc = "Diffview: Refresh" })
+    end,
 }
