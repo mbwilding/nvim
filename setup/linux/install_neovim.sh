6c0834266fd7bf6d4020b5fd497b3b55106302c1
Matthew Wilding <mbwilding@gmail.com>
2024-06-26 18:31:10 +0800

    update

--- setup/linux/install_neovim.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
-chmod +x nvim
-sudo chown root:root nvim
-sudo mv nvim /usr/bin
