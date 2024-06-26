eefcecb54d2f3e985109687bfe9956fbd6ee5e5f
Matthew Wilding <mbwilding@gmail.com>
2024-06-26 23:59:55 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,6 +29,10 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+-- Window split
+vim.keymap.set("n", "<C-S>", "<CMD>split<CR>")
+vim.keymap.set("n", "<C-V>", "<CMD>vsplit<CR>")
+
 -- Tab close
 vim.keymap.set("n", "q", ":tabclose<CR>")
 
