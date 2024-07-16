347d4dec0cd28b92c847e6dcd0a5b72bcec21f6b
Matthew Wilding <mbwilding@gmail.com>
2024-07-16 20:42:19 +0800

    update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,22 +1,17 @@
 return {
-	"folke/which-key.nvim",
-	opts = {},
-	config = function()
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			["<leader>R"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-		})
-		-- register which-key VISUAL mode
-		-- required for visual <leader>hs (hunk stage) to work
-		require("which-key").register({
-			["<leader>"] = { name = "VISUAL <leader>" },
-			["<leader>h"] = { "Git [H]unk" },
-		}, { mode = "v" })
-	end,
+    "folke/which-key.nvim",
+    opts = {},
+    config = function()
+        -- Normal
+        -- require("which-key").register({
+        -- 	["<leader>R"] = { name = "[R]ename", _ = "which_key_ignore" },
+        -- 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+        -- })
+
+        -- Visual
+        -- require("which-key").register({
+        -- 	["<leader>"] = { name = "VISUAL <leader>" },
+        -- 	["<leader>h"] = { "Git [H]unk" },
+        -- }, { mode = "v" })
+    end,
 }
