e03520a61d2003c86b28a1d2780169404adca3b4
Matthew Wilding <mbwilding@gmail.com>
2024-07-18 10:20:31 +0800

    update

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,28 +1,37 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("trouble").setup({})
-
-		vim.keymap.set(
-			"n",
-			"<leader>tD",
-			":TroubleToggle document_diagnostics<CR>",
-			{ desc = "Trouble: Toggle Document Diagnostics" }
-		)
-		vim.keymap.set("n", "<leader>tq", ":TroubleToggle quickfix<CR>", { desc = "Trouble: Quick Fix" })
-		vim.keymap.set("n", "<leader>tl", ":TroubleToggle loclist<CR>", { desc = "Trouble: Toggle Local List" })
-		vim.keymap.set(
-			"n",
-			"<leader>tw",
-			":TroubleToggle workspace_diagnostics<CR>",
-			{ desc = "Trouble: Toggle Workspace Diagnostics" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>tr",
-			":TroubleToggle lsp_references<CR>",
-			{ desc = "Trouble: Toggle LSP References" }
-		)
-	end,
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>td",
+            "<CMD>Trouble diagnostics toggle<CR>",
+            desc = "Trouble: Diagnostics",
+        },
+        {
+            "<leader>tf",
+            "<CMD>Trouble diagnostics toggle filter.buf=0<CR>",
+            desc = "Trouble: Buffer Diagnostics",
+        },
+        {
+            "<leader>ts",
+            "<CMD>Trouble symbols toggle focus=false<CR>",
+            desc = "Trouble: Symbols",
+        },
+        {
+            "<leader>tl",
+            "<CMD>Trouble lsp toggle focus=false win.position=right<CR>",
+            desc = "Trouble: LSP Definitions / References / ...",
+        },
+        {
+            "<leader>tL",
+            "<CMD>Trouble loclist toggle<CR>",
+            desc = "Trouble: Location List",
+        },
+        {
+            "<leader>tQ",
+            "<CMD>Trouble qflist toggle<CR>",
+            desc = "Trouble: Quickfix List",
+        },
+    },
 }
