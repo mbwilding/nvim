cd90cdffa14c59cbe65469a774b31f795645c302
Matthew Wilding <mbwilding@gmail.com>
2024-07-18 18:03:50 +0800

    update

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,24 +2,23 @@
 
 return {
     "NvChad/nvim-colorizer.lua",
-    event = "BufEnter",
     opts = {
         filetypes = { "*" },
         user_default_options = {
-            names = false,    -- "Name" codes like Blue or blue
-            RGB = true,      -- #RGB hex codes
-            RRGGBB = true,   -- #RRGGBB hex codes
-            RRGGBBAA = true, -- #RRGGBBAA hex codes
-            AARRGGBB = false, -- 0xAARRGGBB hex codes
-            rgb_fn = true,  -- CSS rgb() and rgba() functions
-            hsl_fn = true,  -- CSS hsl() and hsla() functions
-            css = true,     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-            css_fn = true,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            names = false,       -- "Name" codes like Blue or blue
+            RGB = true,          -- #RGB hex codes
+            RRGGBB = true,       -- #RRGGBB hex codes
+            RRGGBBAA = true,     -- #RRGGBBAA hex codes
+            AARRGGBB = false,    -- 0xAARRGGBB hex codes
+            rgb_fn = true,       -- CSS rgb() and rgba() functions
+            hsl_fn = true,       -- CSS hsl() and hsla() functions
+            css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+            css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
             -- Available modes for `mode`: foreground, background,  virtualtext
             mode = "background", -- Set the display mode.
             -- Available methods are false / true / "normal" / "lsp" / "both"
             -- True is same as normal
-            tailwind = true,                            -- Enable tailwind colors
+            tailwind = true,                                -- Enable tailwind colors
             -- parsers can contain values used in |user_default_options|
             sass = { enable = true, parsers = { "css" }, }, -- Enable sass colors
             virtualtext = "■",
