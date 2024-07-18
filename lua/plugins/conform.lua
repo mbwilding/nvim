5584fffacd2f21041b73435bf9b7a2278925f3d2
Matthew Wilding <mbwilding@gmail.com>
2024-07-18 19:01:42 +0800

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,55 +1,53 @@
 -- Conform is a formatter manager. It can run multiple formatters and respects the .editorconfig files.
 
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			-- Map of filetype to formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				go = { "goimports", "gofmt" },
-				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				-- You can use a function here to determine the formatters dynamically
-				python = function(bufnr)
-					if require("conform").get_formatter_info("ruff_format", bufnr).available then
-						return { "ruff_format" }
-					else
-						return { "isort", "black" }
-					end
-				end,
-				-- Use the "*" filetype to run formatters on all filetypes
-				["*"] = { "typos" },
-				-- Use the "_" filetype to run formatters on filetypes that don't
-				-- have other formatters configured.
-				["_"] = { "trim_whitespace" },
-			},
-			-- If this is set, Conform will run the formatter on save.
-			-- It will pass the table to conform.format().
-			-- This can also be a function that returns the table.
+    "stevearc/conform.nvim",
+    opts = {
+        -- Map of filetype to formatters
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            go = { "goimports", "gofmt" },
+            -- Use a sub-list to run only the first available formatter
+            javascript = { { "prettierd", "prettier" } },
+            javascriptreact = { { "prettierd", "prettier" } },
+            typescript = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
+            -- You can use a function here to determine the formatters dynamically
+            python = function(bufnr)
+                if require("conform").get_formatter_info("ruff_format", bufnr).available then
+                    return { "ruff_format" }
+                else
+                    return { "isort", "black" }
+                end
+            end,
+            -- Use the "*" filetype to run formatters on all filetypes
+            ["*"] = { "typos" },
+            -- Use the "_" filetype to run formatters on filetypes that don't
+            -- have other formatters configured.
+            ["_"] = { "trim_whitespace" },
+        },
+        -- If this is set, Conform will run the formatter on save.
+        -- It will pass the table to conform.format().
+        -- This can also be a function that returns the table.
 
-			format_on_save = {
-				lsp_fallback = true,
-				timeout_ms = 500,
-			},
+        format_on_save = {
+            lsp_fallback = true,
+            timeout_ms = 500,
+        },
 
-			-- If this is set, Conform will run the formatter asynchronously after save.
-			-- It will pass the table to conform.format().
-			-- This can also be a function that returns the table.
+        -- If this is set, Conform will run the formatter asynchronously after save.
+        -- It will pass the table to conform.format().
+        -- This can also be a function that returns the table.
 
-			-- format_after_save = {
-			-- 	lsp_fallback = true,
-			-- },
+        -- format_after_save = {
+        -- 	lsp_fallback = true,
+        -- },
 
-			-- Set the log level. Use `:ConformInfo` to see the location of the log file.
-			log_level = vim.log.levels.ERROR,
-			-- Conform will notify you when a formatter errors
-			notify_on_error = true,
-			-- Custom formatters and changes to built-in formatters
-		})
-	end,
+        -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+        log_level = vim.log.levels.ERROR,
+        -- Conform will notify you when a formatter errors
+        notify_on_error = true,
+        -- Custom formatters and changes to built-in formatters
+    },
 }
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,46 +1,50 @@
-return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	build = ":Copilot auth",
-	module = "copilot",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<C-y>",
-					accept_word = false,
-					accept_line = false,
-					next = "<C-n>",
-					prev = "<C-p>",
-					dismiss = "<C-Esc>",
-				},
-			},
-			panel = { enabled = true },
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-		})
+-- Copilot integration
 
-		vim.keymap.set("n", "<leader>cpe", "<CMD>Copilot enable<CR>", { desc = "Copilot: Enable" })
-		vim.keymap.set("n", "<leader>cpd", "<CMD>Copilot disable<CR>", { desc = "Copilot: Disable" })
-		vim.keymap.set("n", "<leader>cpt", "<CMD>Copilot toggle<CR>", { desc = "Copilot: Toggle" })
-	end,
-	-- dependencies = {
-	-- 	"zbirenbaum/copilot-cmp",
-	-- 	config = function()
-	-- 		require("copilot_cmp").setup()
-	-- 	end,
-	-- },
+return {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    keys = {
+        {
+            "<leader>cpt",
+            "<CMD>Copilot toggle<CR>",
+            desc = "Copilot: Toggle",
+        },
+        {
+            "<leader>cpe",
+            "<CMD>Copilot enable<CR>",
+            desc = "Copilot: Enable",
+        },
+        {
+            "<leader>cpd",
+            "<CMD>Copilot disable<CR>",
+            desc = "Copilot: Disable",
+        },
+    },
+    opts = {
+        suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            debounce = 75,
+            keymap = {
+                accept = "<C-y>",
+                accept_word = false,
+                accept_line = false,
+                next = "<C-n>",
+                prev = "<C-p>",
+                dismiss = "<C-Esc>",
+            },
+        },
+        panel = { enabled = true },
+        filetypes = {
+            yaml = false,
+            markdown = false,
+            help = false,
+            gitcommit = false,
+            gitrebase = false,
+            hgcommit = false,
+            svn = false,
+            cvs = false,
+            ["."] = false,
+        },
+    },
 }
