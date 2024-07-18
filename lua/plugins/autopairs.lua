ff384dd243e4f343017749fee85791a4db6aae62
Matthew Wilding <mbwilding@gmail.com>
2024-07-18 11:47:20 +0800

    update

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,7 @@
 return {
 	"windwp/nvim-autopairs",
 	dependencies = { "hrsh7th/nvim-cmp" },
-	--event = "InsertEnter",
+	event = "InsertEnter",
 	config = function()
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
