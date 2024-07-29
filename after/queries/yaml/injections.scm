d0108798c56e9a507d653d6c5425d7dcef4e1c95
Matthew Wilding <mbwilding@gmail.com>
2024-07-29 17:30:18 +0800

    update

---
+++ after/queries/yaml/injections.scm
@@ -0,0 +1,8 @@
+; extends
+(block_mapping_pair
+  key: (flow_node) @_pwsh
+  (#any-of? @_pwsh "pwsh")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "powershell")
+    (#offset! @injection.content 0 1 0 0)))
