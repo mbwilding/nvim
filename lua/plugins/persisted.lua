b984bc4364d49c2ecfa08044c0b1c569b16c4c26
Matthew Wilding <mbwilding@gmail.com>
2024-08-21 02:35:19 +0800

    update

--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -1,6 +1,6 @@
 -- Persisted is a session manager
 
-local manual = vim.tbl_contains(vim.v.argv, "+Man!")
+local man = vim.tbl_contains(vim.v.argv, "+Man!")
 
 return {
     "olimorris/persisted.nvim",
@@ -15,11 +15,13 @@ return {
     opts = {
         use_git_branch = false, -- create session files based on the branch of a git enabled repository
         autosave = true,        -- automatically save session files when exiting Neovim
-        autoload = not manual,  -- automatically load the session for the cwd on Neovim startup
+        autoload = not man,     -- automatically load the session for the cwd on Neovim startup
         silent = true,          -- No message on loading a session
         follow_cwd = true,      -- follow the cwd of the session file
         should_autosave = function()
-            return vim.bo.filetype ~= "alpha" and not manual and vim.fn.getcwd() ~= os.getenv("HOME")
+            return vim.bo.filetype ~= "alpha"
+            and vim.fn.getcwd() ~= os.getenv("HOME")
+            and not man
         end,
     }
 }
