df5c727c4089eeac860a1d9903ded77c4e286c32
Matthew Wilding <mbwilding@gmail.com>
2024-08-24 02:36:34 +0800

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,95 +1,97 @@
 -- CMP provides autocomplete suggestions in a pop-up menu.
 
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets
-				-- This step is not supported in many windows environments
-				-- Remove the below condition to re-enable on windows
-				if vim.fn.has("win32") == 1 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-		},
-		"saadparwaiz1/cmp_luasnip",
-		-- Adds LSP completion capabilities
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		-- Adds a number of user-friendly snippets
-		"rafamadriz/friendly-snippets",
-		"onsails/lspkind.nvim",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
-		require("luasnip.loaders.from_vscode").lazy_load()
-		luasnip.config.setup()
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        {
+            "L3MON4D3/LuaSnip",
+            build = (function()
+                -- Build Step is needed for regex support in snippets
+                -- This step is not supported in many windows environments
+                -- Remove the below condition to re-enable on windows
+                if vim.fn.has("win32") == 1 then
+                    return
+                end
+                return "make install_jsregexp"
+            end)(),
+        },
+        "saadparwaiz1/cmp_luasnip",
+        -- Adds LSP completion capabilities
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        -- Adds a number of user-friendly snippets
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim",
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			completion = {
-				completeopt = "menu,menuone,noinsert",
-			},
-			mapping = {
-				-- Select the [n]ext item
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<C-p>"] = cmp.mapping.select_prev_item(),
+        -- "zbirenbaum/icopilot-cmp"
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
+        require("luasnip.loaders.from_vscode").lazy_load()
+        luasnip.config.setup()
 
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            completion = {
+                completeopt = "menu,menuone,noinsert",
+            },
+            mapping = {
+                -- Select the [n]ext item
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                -- Select the [p]revious item
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
 
-				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
-				["<C-Space>"] = cmp.mapping.complete({}),
+                -- Accept ([y]es) the completion.
+                --  This will auto-import if your LSP supports it.
+                --  This will expand snippets if the LSP sent a snippet.
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
+                -- Manually trigger a completion from nvim-cmp.
+                --  Generally you don't need this, because nvim-cmp will display
+                --  completions whenever it has completion options available.
+                ["<C-Space>"] = cmp.mapping.complete({}),
 
-				-- ["<C-l>"] = cmp.mapping(function()
-				-- 	if luasnip.expand_or_locally_jumpable() then
-				-- 		luasnip.expand_or_jump()
-				-- 	end
-				-- end, { "i", "s" }),
-				-- ["<C-h>"] = cmp.mapping(function()
-				-- 	if luasnip.locally_jumpable(-1) then
-				-- 		luasnip.jump(-1)
-				-- 	end
-				-- end, { "i", "s" }),
-			},
-			sources = {
-				-- { name = "copilot", group_index = 2 },
-				{ name = "nvim_lsp", group_index = 2 },
-				{ name = "path",     group_index = 2 },
-				{ name = "luasnip",  group_index = 2 },
-			},
-			formatting = {
-				format = lspkind.cmp_format({
-					max_width = 50,
-					-- mode = "symbol",
-					-- symbol_map = { Copilot = "" },
-				}),
-			},
-		})
-	end,
+                -- Think of <c-l> as moving to the right of your snippet expansion.
+                --  So if you have a snippet that's like:
+                --  function $name($args)
+                --    $body
+                --  end
+                --
+                -- <c-l> will move you to the right of each of the expansion locations.
+                -- <c-h> is similar, except moving you backwards.
+
+                -- ["<C-l>"] = cmp.mapping(function()
+                -- 	if luasnip.expand_or_locally_jumpable() then
+                -- 		luasnip.expand_or_jump()
+                -- 	end
+                -- end, { "i", "s" }),
+                -- ["<C-h>"] = cmp.mapping(function()
+                -- 	if luasnip.locally_jumpable(-1) then
+                -- 		luasnip.jump(-1)
+                -- 	end
+                -- end, { "i", "s" }),
+            },
+            sources = {
+                { name = "nvim_lsp", group_index = 2 },
+                { name = "path",     group_index = 2 },
+                { name = "luasnip",  group_index = 2 },
+                -- { name = "copilot",  group_index = 2 },
+            },
+            formatting = {
+                format = lspkind.cmp_format({
+                    max_width = 50,
+                    -- mode = "symbol",
+                    -- symbol_map = { Copilot = "" },
+                }),
+            },
+        })
+    end,
 }
