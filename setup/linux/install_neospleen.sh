8e3960f8ddd87ccf12ef4766aa721e6136caf4bd
Matthew Wilding <mbwilding@gmail.com>
2024-09-10 15:29:04 +0800

    update

--- setup/linux/install_neospleen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-font_path=$HOME/.local/share/fonts
-mkdir -p $font_path
-pushd $font_path
-rm NeoSpleen*.ttf
-wget https://github.com/mbwilding/NeoSpleen/releases/latest/download/NeoSpleen-NerdFont.ttf
-popd
-fc-cache -f
--- setup/mac/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-brew install neovim node python go ripgrep fzf
