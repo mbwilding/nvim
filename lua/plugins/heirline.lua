684f1c6142c6e4ab8ee3b1400875370e2532fcc8
Matthew Wilding <mbwilding@gmail.com>
2024-09-29 15:42:26 +0800

    update

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -10,7 +10,7 @@ return {
 
         local conditions = require("heirline.conditions")
         local utils = require("heirline.utils")
-        local colors = require("gronk.colors.wilding")
+        local colors = require("gronk.colors." .. vim.g.colors_name:match("([^%-]+)$"))
         require("heirline").load_colors(colors)
 
         local icons = true
