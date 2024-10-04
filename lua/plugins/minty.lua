550bef1a86c1bae0de446f94ef38447e3a29a75c
Matthew Wilding <mbwilding@gmail.com>
2024-10-04 10:27:27 +0800

    update

---
+++ lua/plugins/minty.lua
@@ -0,0 +1,28 @@
+return {
+    "nvchad/minty",
+    lazy = true,
+    keys = {
+        {
+            "<leader>mih",
+            function()
+                require("minty.huefy").open()
+            end,
+            desc = "Minty: Huefy",
+            mode = "n",
+        },
+        {
+            "<leader>mis",
+            function()
+                require("minty.shades").open()
+            end,
+            desc = "Minty: Shades",
+            mode = "n",
+        },
+    },
+    dependencies = {
+        {
+            "nvchad/volt",
+            lazy = true,
+        }
+    },
+}
