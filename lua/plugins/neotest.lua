585de6e407d4592a9c4aa2adeb43b277d69f8711
Matthew Wilding <mbwilding@gmail.com>
2024-10-06 21:22:43 +0800

    update

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -9,6 +9,64 @@ return {
         "rouge8/neotest-rust",
         "Issafalcon/neotest-dotnet",
     },
+    keys = {
+        {
+            "<leader>ntr",
+            function()
+                require("neotest").run.run()
+            end,
+            desc = "Neotest: Run Nearest Test",
+        },
+        {
+            "<leader>ntf",
+            function()
+                require("neotest").run.run(vim.fn.expand("%"))
+            end,
+            desc = "Neotest: Run Current File",
+        },
+        {
+            "<leader>ntd",
+            function()
+                require("neotest").run.run({ strategy = "dap" })
+            end,
+            desc = "Neotest: Debug Nearest Test",
+        },
+        {
+            "<leader>nts",
+            function()
+                require("neotest").run.stop()
+            end,
+            desc = "Neotest: Stop Nearest Test",
+        },
+        {
+            "<leader>nta",
+            function()
+                require("neotest").run.attach()
+            end,
+            desc = "Neotest: Attach Nearest Test",
+        },
+        {
+            "<leader>ntw",
+            function()
+                require("neotest").watch.watch()
+            end,
+            desc = "Neotest: Watch",
+        },
+        {
+            "<leader>ntt",
+            function()
+                require("neotest").watch.toggle()
+            end,
+            desc = "Neotest: Watch Toggle",
+        },
+        {
+            "<leader>ntW",
+            function()
+                require("neotest").watch.toggle(vim.fn.expand("%"))
+            end,
+            desc = "Neotest: Watch File",
+        },
+    },
     config = function()
         require("neotest").setup({
             adapters = {
@@ -21,37 +79,5 @@ return {
                 }),
             },
         })
-
-        vim.keymap.set("n", "<leader>ntr", function()
-            require("neotest").run.run()
-        end, { desc = "Neotest: Run Nearest Test" })
-
-        vim.keymap.set("n", "<leader>ntf", function()
-            require("neotest").run.run(vim.fn.expand("%"))
-        end, { desc = "Neotest: Run Current File" })
-
-        vim.keymap.set("n", "<leader>ntd", function()
-            require("neotest").run.run({ strategy = "dap" })
-        end, { desc = "Neotest: Debug Nearest Test" })
-
-        vim.keymap.set("n", "<leader>nts", function()
-            require("neotest").run.stop()
-        end, { desc = "Neotest: Stop Nearest Test" })
-
-        vim.keymap.set("n", "<leader>nta", function()
-            require("neotest").run.attach()
-        end, { desc = "Neotest: Attach Nearest Test" })
-
-        vim.keymap.set("n", "<leader>ntw", function()
-            require("neotest").watch.watch()
-        end, { desc = "Neotest: Watch" })
-
-        vim.keymap.set("n", "<leader>ntt", function()
-            require("neotest").watch.toggle()
-        end, { desc = "Neotest: Watch Toggle" })
-
-        vim.keymap.set("n", "<leader>ntW", function()
-            require("neotest").watch.toggle(vim.fn.expand("%"))
-        end, { desc = "Neotest: Watch File" })
     end,
 }
