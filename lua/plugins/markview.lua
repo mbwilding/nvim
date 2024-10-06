7af89497f5f0052a37c5ab3bca1a7fc9e2baa7bd
Matthew Wilding <mbwilding@gmail.com>
2024-10-06 17:19:43 +0800

    update

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -6,4 +6,24 @@ return {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+        local presets = require("markview.presets")
+
+        require("markview").setup({
+            block_quotes = {},
+            callbacks = {},
+            checkboxes = presets.checkboxes.nerd,
+            code_blocks = {},
+            escaped = {},
+            footnotes = {},
+            -- headings = presets.headings.arrowed,
+            horizontal_rules = presets.horizontal_rules.thick,
+            html = {},
+            inline_codes = {},
+            latex = {},
+            links = {},
+            list_items = {},
+            tables = {}
+        })
+    end
 }
