849bb7f2ef70d398a55cae357215a7dedfa1852e
Matthew Wilding <mbwilding@gmail.com>
2024-10-18 14:04:57 +0800

    update

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -8,7 +8,15 @@ return {
     config = function()
         local cmp_autopairs = require("nvim-autopairs.completion.cmp")
         local cmp = require("cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        local handlers = require('nvim-autopairs.completion.handlers')
+        cmp.event:on(
+            "confirm_done",
+            cmp_autopairs.on_confirm_done({
+                filetypes = {
+                    rust = false,
+                }
+            })
+        )
         require("nvim-autopairs").setup({
             check_ts = true, -- Enable treesitter
             ts_config = {
