1ebcc270caf149d19e1a7c837a01fc18c8b800cb
Matthew Wilding <mbwilding@gmail.com>
2024-12-16 12:00:03 +0800

    update

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,5 +1,7 @@
+-- Blink is an completion plugin written in Rust
+
 return {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     lazy = false, -- lazy loading handled internally
     -- optional: provides snippets for the snippet source
     dependencies = {
@@ -8,23 +10,23 @@ return {
     },
 
     -- use a release tag to download pre-built binaries
-    version = 'v0.*',
+    version = "v0.*",
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
+    -- build = "cargo build --release",
     -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    -- build = "nix run .#build-plugin",
 
-    ---@module 'blink.cmp'
+    ---@module "blink.cmp"
     ---@type blink.cmp.Config
     opts = {
-        -- 'default' for mappings similar to built-in completion
-        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+        -- "default" for mappings similar to built-in completion
+        -- "super-tab" for mappings similar to vscode (tab to accept, arrow keys to navigate)
+        -- "enter" for mappings similar to "super-tab" but with "enter" to accept
         -- see the "default configuration" section below for full documentation on how to define
         -- your own keymap.
 
         -- keymap = {
-        --     preset = 'default',
+        --     preset = "default",
         --     ["<A-1>"] = { function(cmp) cmp.accept({ index = 1 }) end },
         --     ["<A-2>"] = { function(cmp) cmp.accept({ index = 2 }) end },
         --     ["<A-3>"] = { function(cmp) cmp.accept({ index = 3 }) end },
@@ -41,20 +43,20 @@ return {
             -- Useful for when your theme doesn't support blink.cmp
             -- will be removed in a future release
             use_nvim_cmp_as_default = true,
-            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Set to "mono" for "Nerd Font Mono" or "normal" for "Nerd Font"
             -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'mono'
+            nerd_font_variant = "mono"
         },
 
         snippets = {
-            expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+            expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
             active = function(filter)
                 if filter and filter.direction then
-                    return require('luasnip').jumpable(filter.direction)
+                    return require("luasnip").jumpable(filter.direction)
                 end
-                return require('luasnip').in_snippet()
+                return require("luasnip").in_snippet()
             end,
-            jump = function(direction) require('luasnip').jump(direction) end,
+            jump = function(direction) require("luasnip").jump(direction) end,
         },
 
         -- default list of enabled providers defined so that you can extend it
@@ -106,7 +108,7 @@ return {
         },
 
         -- experimental signature help support
-        -- signature = { enabled = true }
+        signature = { enabled = true },
     },
     -- allows extending the enabled_providers array elsewhere in your config
     -- without having to redefine it
