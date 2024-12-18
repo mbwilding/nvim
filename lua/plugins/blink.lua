6da41a9a463d52ed0fb931f3516fec454a2e092f
Matthew Wilding <mbwilding@gmail.com>
2024-12-18 23:49:29 +0800

    update

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -9,42 +9,15 @@ return {
         "L3MON4D3/LuaSnip",
     },
 
-    -- use a release tag to download pre-built binaries
     version = "v0.*",
-    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = "cargo build --release",
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = "nix run .#build-plugin",
 
     ---@module "blink.cmp"
     ---@type blink.cmp.Config
     opts = {
-        -- "default" for mappings similar to built-in completion
-        -- "super-tab" for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- "enter" for mappings similar to "super-tab" but with "enter" to accept
-        -- see the "default configuration" section below for full documentation on how to define
-        -- your own keymap.
-
-        -- keymap = {
-        --     preset = "default",
-        --     ["<A-1>"] = { function(cmp) cmp.accept({ index = 1 }) end },
-        --     ["<A-2>"] = { function(cmp) cmp.accept({ index = 2 }) end },
-        --     ["<A-3>"] = { function(cmp) cmp.accept({ index = 3 }) end },
-        --     ["<A-4>"] = { function(cmp) cmp.accept({ index = 4 }) end },
-        --     ["<A-5>"] = { function(cmp) cmp.accept({ index = 5 }) end },
-        --     ["<A-6>"] = { function(cmp) cmp.accept({ index = 6 }) end },
-        --     ["<A-7>"] = { function(cmp) cmp.accept({ index = 7 }) end },
-        --     ["<A-8>"] = { function(cmp) cmp.accept({ index = 8 }) end },
-        --     ["<A-9>"] = { function(cmp) cmp.accept({ index = 9 }) end },
-        -- },
+        keymap = { preset = "default" },
 
         appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release
             use_nvim_cmp_as_default = false,
-            -- Set to "mono" for "Nerd Font Mono" or "normal" for "Nerd Font"
-            -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = "mono"
         },
 
@@ -131,7 +104,6 @@ return {
             }
         },
 
-        -- experimental signature help support
         signature = {
             enabled = true
         },
