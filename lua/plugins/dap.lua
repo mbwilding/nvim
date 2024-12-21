94ac868230307c658c2f7e3af29b5d8ed9cff7af
Matthew Wilding <mbwilding@gmail.com>
2024-12-22 03:55:57 +0800

    2024-12-22-03-55-56

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -93,14 +93,14 @@ return {
 
         -- Adapters
         dap.adapters.codelldb = {
+            name = "codelldb",
             type = "server",
             port = "${port}",
             executable = {
                 command = vim.fn.exepath("codelldb"),
                 args = { "--port", "${port}" },
                 detached = vim.fn.has('win32') == 0
-            },
-            name = "codelldb"
+            }
         }
 
         dap.adapters.coreclr = {
