47d759ff0a87bd8095072d779d3d13f1dae89516
Matthew Wilding <mbwilding@gmail.com>
2024-12-22 12:11:18 +0800

    2024-12-22-12-11-18

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -72,7 +72,8 @@ return {
         vim.keymap.set("n", "<F3>", dap.step_over, { desc = "Debug: Step Over" })
         vim.keymap.set("n", "<F4>", dap.step_out, { desc = "Debug: Step Out" })
         vim.keymap.set("n", "<F5>", dap.step_back, { desc = "Debug: Step Back" })
-        vim.keymap.set("n", "<F6>", dap.run_to_cursor, { desc = "Debug: Run To Cursor" })
+        vim.keymap.set("n", "<F6>", function() dap.disconnect({ terminateDebuggee = true }) end, { desc = "Debug: Terminate" })
+        vim.keymap.set("n", "<F7>", dap.run_to_cursor, { desc = "Debug: Run To Cursor" })
 
         vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
         vim.keymap.set("n", "<leader>B", function()
