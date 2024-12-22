beeda71ffb6f1b9e55f262f68b6435561e3f7298
Matthew Wilding <mbwilding@gmail.com>
2024-12-22 14:57:26 +0800

    update

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -4,7 +4,6 @@ vim.g.neovide_cursor_trail_size = 0.1
 vim.g.neovide_cursor_vfx_mode = "sonicboom" -- railgun, torpedo, pixiedust, sonicboom, ripple, wireframe
 
 vim.o.guifont = "NeoSpleen Nerd Font:h17:w-1"
-vim.opt.linespace = 1
 
 vim.g.neovide_padding_top = 0
 vim.g.neovide_padding_bottom = 0
