92e0fbf4041f6879a1df85d65a5574f34dc42da3
Matthew Wilding <mbwilding@gmail.com>
2024-12-22 11:24:20 +0800

    2024-12-22-11-24-20

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -32,15 +32,15 @@ return {
         })
 
         require("nvim-dap-virtual-text").setup({
-            enabled = true,                         -- enable this plugin (the default)
-            enabled_commands = true,                -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-            highlight_changed_variables = true,     -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-            highlight_new_as_changed = false,       -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-            show_stop_reason = true,                -- show stop reason when stopped for exceptions
-            commented = false,                      -- prefix virtual text with comment string
-            only_first_definition = false,          -- only show virtual text at first definition (if there are multiple)
-            all_references = true,                  -- show virtual text on all all references of the variable (not only definitions)
-            clear_on_continue = false,              -- clear virtual text on "continue" (might cause flickering when stepping)
+            enabled = true,                     -- enable this plugin (the default)
+            enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+            highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+            highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+            show_stop_reason = true,            -- show stop reason when stopped for exceptions
+            commented = false,                  -- prefix virtual text with comment string
+            only_first_definition = false,      -- only show virtual text at first definition (if there are multiple)
+            all_references = true,              -- show virtual text on all all references of the variable (not only definitions)
+            clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
             --- A callback that determines how a variable is displayed or whether it should be omitted
             --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
             --- @param buf number
@@ -60,9 +60,9 @@ return {
             virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
 
             -- experimental features:
-            all_frames = false,         -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-            virt_lines = false,         -- show virtual lines instead of virtual text (will flicker!)
-            virt_text_win_col = 80      -- position the virtual text at a fixed window column (starting from the first text column)
+            all_frames = false,    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+            virt_lines = false,    -- show virtual lines instead of virtual text (will flicker!)
+            virt_text_win_col = 80 -- position the virtual text at a fixed window column (starting from the first text column)
         })
 
         -- Basic debugging keymaps, feel free to change to your liking!
@@ -210,7 +210,8 @@ return {
                             return error("Project directory not found")
                         end
 
-                        local result = vim.fn.systemlist("cargo read-manifest --manifest-path " .. project_dir .. "/Cargo.toml")
+                        local result = vim.fn.systemlist("cargo read-manifest --manifest-path " ..
+                            project_dir .. "/Cargo.toml")
                         if vim.v.shell_error ~= 0 then
                             return error("Failed to read Cargo.toml")
                         end
