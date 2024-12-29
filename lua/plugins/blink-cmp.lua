39d12aa54023ee7b6955fc6b7e3683eb64c1e751
Matthew Wilding <mbwilding@gmail.com>
2024-12-30 00:53:04 +0800

    update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -51,17 +51,17 @@ return {
 
         keymap = {
             preset = "default",
-            ["<Tab>"] = { "snippet_forward", "fallback" },
-            ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-            ["<C-p>"] = { "select_prev", "fallback" },
-            ["<C-n>"] = { "select_next", "fallback" },
-
-            ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-            ["K"] = { "show", "show_documentation", "hide_documentation" },
-            ["<C-e>"] = { "hide", "fallback" },
+            -- ["<Tab>"] = { "snippet_forward", "fallback" },
+            -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
+            --
+            -- ["<C-p>"] = { "select_prev", "fallback" },
+            -- ["<C-n>"] = { "select_next", "fallback" },
+            --
+            -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+            -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+            --
+            -- ["K"] = { "show", "show_documentation", "hide_documentation" },
+            -- ["<C-e>"] = { "hide", "fallback" },
         },
 
         appearance = {
