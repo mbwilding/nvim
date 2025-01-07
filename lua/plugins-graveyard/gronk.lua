22d78fab90cf9eb8d1583010765b727381ae9c2e
Matthew Wilding <mbwilding@gmail.com>
2025-01-07 17:23:05 +0800

    update

---
+++ lua/plugins-graveyard/gronk.lua
@@ -0,0 +1,14 @@
+-- Custom theme based on the Rider Dark theme
+
+return {
+    "mbwilding/gronk.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("gronk").setup({
+            transparent = true,
+        })
+
+        vim.cmd([[colorscheme gronk-wilding]])
+    end,
+}
