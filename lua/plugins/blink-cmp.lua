68f6e48e778a4c8799f3c75548ba4c6f7463ac9d
Matthew Wilding <mbwilding@gmail.com>
2025-01-08 12:05:52 +0800

    update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -111,4 +111,31 @@ return {
             enabled = true,
         },
     },
+    config = function(_, opts)
+        local luasnip = require("luasnip")
+        luasnip.config.setup()
+
+        local s = luasnip.snippet
+        local sn = luasnip.snippet_node
+        local t = luasnip.text_node
+        local i = luasnip.insert_node
+        local f = luasnip.function_node
+        local c = luasnip.choice_node
+        local d = luasnip.dynamic_node
+        local r = luasnip.restore_node
+        luasnip.add_snippets("rust", {
+            -- s("struct", {
+            --     t({ "#[derive(Debug)]", "struct " }), i(1, "Name"), t({ " {", "\t" }), i(2, "field"), t({ ": "}), i(3, "Type"), t({ "", "}" }),
+            -- }),
+            s("impl-struct", {
+                t({ "impl " }),
+                i(1, "Name"),
+                t({ " {", "\tpub fn new() -> Self {", "\t\tSelf {", "\t\t\t" }),
+                i(2, "// init_fields"),
+                t({ "", "\t\t}", "\t}", "}" }),
+            }),
+        })
+
+        require("blink-cmp").setup(opts)
+    end
 }
