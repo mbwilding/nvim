eebfe1bcb3158b7a2a34ff4063284bb957a6bbf8
Matthew Wilding <mbwilding@gmail.com>
2025-01-10 08:57:09 +0800

    update

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,109 +1,115 @@
+local g = vim.g
+local o = vim.opt
+local wo = vim.wo
+
 -- Enable loading plugins faster
 vim.loader.enable()
 
 -- Set leader key to space
-vim.g.mapleader = " "
+local leader = " "
+g.mapleader = leader
+g.maplocalleader = leader
 
 -- Nerd font
-vim.g.have_nerd_font = true
+g.have_nerd_font = true
 
 -- Enable true color support
-vim.o.termguicolors = true
+o.termguicolors = true
 
 -- Dark theme
-vim.o.background = "dark"
+o.background = "dark"
 
 -- Highlight search results
-vim.o.hlsearch = true
+o.hlsearch = true
 
 -- Gutter line numbers
-vim.wo.number = true
+wo.number = true
 
 -- Gutter relative line numbers
-vim.wo.relativenumber = false
+wo.relativenumber = false
 
 -- Gutter sign column
-vim.wo.signcolumn = "yes:2"
+wo.signcolumn = "yes:2"
 
 -- Mouse mode
-vim.o.mouse = "a"
-vim.o.mousemodel = ""
+o.mouse = "a"
+o.mousemodel = ""
 
 -- Enable break indent
-vim.o.breakindent = true
+o.breakindent = true
 
 -- Enable undo file
-vim.o.undofile = true
+o.undofile = true
 
 -- Ignore case in search
-vim.o.ignorecase = true
+o.ignorecase = true
 
 -- Enable smart case
-vim.o.smartcase = true
+o.smartcase = true
 
 -- Set update time for diagnostics
-vim.o.updatetime = 250
+o.updatetime = 250
 
 -- Leader command timeout
-vim.o.timeout = true
-vim.o.timeoutlen = 300
+o.timeout = true
+o.timeoutlen = 300
 
 -- Set completion options
-vim.o.completeopt = "menuone,noselect"
+o.completeopt = "menuone,noselect"
 
 -- Allow backspacing over everything
-vim.o.backspace = "indent,eol,start"
+o.backspace = "indent,eol,start"
 
 -- Enable smart indent
-vim.o.smartindent = true
+o.smartindent = true
 
 -- Text wrapping
-vim.o.wrap = true
+o.wrap = true
 
 -- Disable swap file
-vim.o.swapfile = false
+o.swapfile = false
 
 -- Disable backup file
-vim.o.backup = false
+o.backup = false
 
 -- Set undo levels
-vim.o.undolevels = 30000
+o.undolevels = 30000
 
 -- Set scroll offset
-vim.o.scrolloff = 10
+o.scrolloff = 10
 
 -- Update time for CursorHold
-vim.o.updatetime = 50
+o.updatetime = 50
 
 -- Enable auto write
-vim.o.autowrite = true
+o.autowrite = true
 
 -- Disable cursor line
-vim.o.cursorline = false
+o.cursorline = false
 
 -- Enable confirm dialog when leaving buffer
-vim.o.confirm = true
+o.confirm = true
 
 -- Line break
-vim.o.linebreak = true
+o.linebreak = true
 
 -- Spell checking
-vim.o.spelllang = "en_au"
--- vim.o.spell = true
+o.spelllang = "en_au"
+-- o.spell = true
 
 -- Set fill characters after EOF
-vim.opt.fillchars:append({ eob = " " })
+o.fillchars:append({ eob = " " })
 
 -- Indentation
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
+o.shiftwidth = 4
+o.tabstop = 4
 
 -- Splits
-vim.o.splitright = true
-vim.o.splitbelow = true
+o.splitright = true
+o.splitbelow = true
 
 -- Single status line
-vim.o.laststatus = 3
+o.laststatus = 3
 
 -- TODO: MOVE
 -- Icons
