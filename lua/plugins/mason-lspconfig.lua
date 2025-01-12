70444c7b1f802fe708c3711a7a333692f8cce3bf
Matthew Wilding <mbwilding@gmail.com>
2025-01-12 21:34:09 +0800

    update

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -443,9 +443,9 @@ return {
             -- The first entry (without a key) will be the default handler
             -- and will be called for each installed server that doesn't have
             -- a dedicated handler.
-            function(server_name) -- default handler
+            function(server_name)
                 if not servers[server_name] then
-                    require("lspconfig")[server_name].setup({})
+                    require("lspconfig")[server_name].setup()
                 end
             end,
         })
