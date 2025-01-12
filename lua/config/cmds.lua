7fcc0a087733bce8018f93bf0e851506a6388c7f
Matthew Wilding <mbwilding@gmail.com>
2025-01-12 17:07:09 +0800

    2025-01-12-17-07-09

--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -2,14 +2,14 @@ vim.api.nvim_create_user_command("Push", function()
     vim.fn.jobstart("git add . && git commit -m \"" .. os.date("%Y-%m-%d-%H-%M-%S") .. "\" && git push", {
         on_stdout = function(_, data)
             if data then
-                vim.api.nvim_out_write(table.concat(data, "\n") .. "\n")
+                vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
             end
         end,
         on_exit = function(_, code, _)
             if code == 0 then
-                vim.api.nvim_out_write("Push successful\n")
+                vim.notify("Push successful", vim.log.levels.INFO)
             else
-                vim.api.nvim_err_writeln("Push failed")
+                vim.notify("Push failed", vim.log.levels.ERROR)
             end
         end,
     })
