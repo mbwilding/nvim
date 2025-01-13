2b7589777989f2ecf8a7b592e168993ac05c95f6
Matthew Wilding <mbwilding@gmail.com>
2025-01-13 20:52:23 +0800

    update

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -10,33 +10,34 @@ return {
         "Hoffs/omnisharp-extended-lsp.nvim",
         "kevinhwang91/nvim-ufo",
         -- "Decodetalkers/csharpls-extended-lsp.nvim",
-        {
-            "seblj/roslyn.nvim",
-            ft = "cs",
-            opts = {
-                config = {
-                    settings = {
-                        ["csharp|inlay_hints"] = {
-                            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-                            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-                            csharp_enable_inlay_hints_for_types = true,
-                            dotnet_enable_inlay_hints_for_indexer_parameters = true,
-                            dotnet_enable_inlay_hints_for_literal_parameters = true,
-                            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-                            dotnet_enable_inlay_hints_for_other_parameters = true,
-                            dotnet_enable_inlay_hints_for_parameters = true,
-                            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-                            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-                            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-                        },
-                        ["csharp|code_lens"] = {
-                            dotnet_enable_references_code_lens = true,
-                        },
-                    },
-                },
-            },
-        },
+
+        -- {
+        --     "seblj/roslyn.nvim",
+        --     ft = "cs",
+        --     opts = {
+        --         config = {
+        --             settings = {
+        --                 ["csharp|inlay_hints"] = {
+        --                     csharp_enable_inlay_hints_for_implicit_object_creation = true,
+        --                     csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        --                     csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+        --                     csharp_enable_inlay_hints_for_types = true,
+        --                     dotnet_enable_inlay_hints_for_indexer_parameters = true,
+        --                     dotnet_enable_inlay_hints_for_literal_parameters = true,
+        --                     dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+        --                     dotnet_enable_inlay_hints_for_other_parameters = true,
+        --                     dotnet_enable_inlay_hints_for_parameters = true,
+        --                     dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+        --                     dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+        --                     dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+        --                 },
+        --                 ["csharp|code_lens"] = {
+        --                     dotnet_enable_references_code_lens = true,
+        --                 },
+        --             },
+        --         },
+        --     },
+        -- },
     },
     config = function()
         -- on_attach function to overwrite the default keymaps
@@ -292,7 +293,7 @@ return {
             --     },
             -- },
             omnisharp = {
-                enabled = false,
+                enabled = true,
                 settings = {
                     csharp = {
                         inlayHints = {
