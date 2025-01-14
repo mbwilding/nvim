2740c1b0f1d95dd9d673fa2e814ccee191cdc756
Matthew Wilding <mbwilding@gmail.com>
2025-01-14 17:43:09 +0800

    update

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,41 +12,45 @@ return {
         -- "hrsh7th/nvim-cmp",
     },
     config = function()
-        require("noice").setup({
-            cmdline = {
-                enabled = true,
-                view = "cmdline",
-            },
-            messages = {
-                enabled = true, -- enables the Noice messages UI
-                -- view = "notify", -- default view for messages
-                -- view_error = "notify", -- view for errors
-                -- view_warn = "notify", -- view for warnings
-                view_history = "messages",   -- view for :messages
-                view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-            },
-            -- popupmenu = {
-            --     enabled = false,
-            -- },
-            lsp = {
-                progress = {
+        require("noice").setup(
+            {
+                cmdline = {
                     enabled = true,
+                    view = "cmdline",
+                    format = {
+                        input = { view = "cmdline", icon = "󰥻 " },
+                    },
                 },
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                messages = {
+                    enabled = true, -- enables the Noice messages UI
+                    -- view = "notify", -- default view for messages
+                    -- view_error = "notify", -- view for errors
+                    -- view_warn = "notify", -- view for warnings
+                    view_history = "messages",   -- view for :messages
+                    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
                 },
-            },
-            presets = {
-                bottom_search = true,         -- use a classic bottom cmdline for search
-                command_palette = true,       -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = true,        -- add a border to hover docs and signature help
-            },
-        })
+                -- popupmenu = {
+                --     enabled = false,
+                -- },
+                lsp = {
+                    progress = {
+                        enabled = true,
+                    },
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    -- override = {
+                    --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    --     ["vim.lsp.util.stylize_markdown"] = true,
+                    --     -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    -- },
+                },
+                presets = {
+                    bottom_search = true,         -- use a classic bottom cmdline for search
+                    command_palette = true,       -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = true,        -- add a border to hover docs and signature help
+                },
+            })
 
         vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Noice: Dismiss" })
     end,
