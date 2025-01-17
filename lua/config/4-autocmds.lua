d449b0c7c65e47df9526b4205af37ea5503d90b5
Matthew Wilding <mbwilding@gmail.com>
2025-01-17 19:26:22 +0800

    update

--- lua/config/4-autocmds.lua
+++ lua/config/4-autocmds.lua
@@ -68,7 +68,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     group = augroup("lf"),
     pattern = "*.sh",
-    command = "setlocal fileformat=unix",
+    command = "set fileformat=unix",
 })
 
 -- Virtual text only on current line
@@ -96,6 +96,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
                 })
             end,
         })
+    end,
+})
+
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.cmd('normal! g`"zz')
+        end
     end,
 })
 
@@ -167,16 +178,5 @@ vim.api.nvim_create_autocmd("LspAttach", {
         --         end,
         --     })
         -- end
-    end,
-})
-
--- Restore cursor to file position in previous editing session
-vim.api.nvim_create_autocmd("BufReadPost", {
-    callback = function(args)
-        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
-        local line_count = vim.api.nvim_buf_line_count(args.buf)
-        if mark[1] > 0 and mark[1] <= line_count then
-            vim.cmd('normal! g`"zz')
-        end
     end,
 })
