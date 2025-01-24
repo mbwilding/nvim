5471e627dd83cab4af2a5374a7aebaf64023f103
Matthew Wilding <mbwilding@gmail.com>
2025-01-24 19:35:14 +0800

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-local config_files = vim.fn.glob("./lua/config/*.lua", false, true)
-for _, file in ipairs(config_files) do
-    require("config." .. file:match("([^/]+)%.lua$"))
+for _, f in ipairs(vim.fn.glob("./lua/config/*.lua", false, true)) do
+    require("config." .. f:match("([^/]+)%.lua$"))
 end
