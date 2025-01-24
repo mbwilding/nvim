4fcec9ee5e62d0fd6cdfef8927e677d5a92bf767
Matthew Wilding <mbwilding@gmail.com>
2025-01-24 10:33:30 +0800

    update

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -203,7 +203,7 @@ return {
         local file_encoding = {
             provider = function()
                 local x = vim.bo.fenc
-                local has_bom = vim.bo.bomb and "[BOM]" or ""
+                local has_bom = vim.bo.bomb and "-BOM" or ""
                 if x ~= "" then
                     return x:upper() .. has_bom
                 end
