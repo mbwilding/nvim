0fe6cce5091af66c61d62ed21c51dc53ba7bc9d0
Matthew Wilding <mbwilding@gmail.com>
2025-01-24 19:51:34 +0800

    update

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,15 +1,15 @@
 local modules = {
-  "settings",
-  "signs",
-  "keymaps",
-  "autocmds",
-  "cmds",
-  "filetypes",
-  "platform",
-  "neovide",
-  "lazy",
+    "settings",
+    "signs",
+    "keymaps",
+    "autocmds",
+    "cmds",
+    "filetypes",
+    "platform",
+    "neovide",
+    "lazy",
 }
 
 for _, module in ipairs(modules) do
-  require("config/" .. module)
+    require("config/" .. module)
 end
