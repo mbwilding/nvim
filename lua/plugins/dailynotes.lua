f2542bcde01c3654fbdd7165b3b33dbb6aaeda4c
Matthew Wilding <mbwilding@gmail.com>
2025-01-25 13:56:14 +0800

    update

--- lua/plugins/dailynotes.lua
+++ lua/plugins/dailynotes.lua
@@ -1,9 +1,6 @@
 return {
     "kperath/dailynotes.nvim",
     lazy = true,
-    opts = {
-        path = "~/notes/",
-    },
     keys = {
         {
             "<leader>dnt",
@@ -21,42 +18,41 @@ return {
             desc = "DailyNotes: Prev",
         },
     },
+    opts = {
+        path = "~/notes/",
+    },
     config = function(_, opts)
         require("dailynotes").setup(opts)
 
         -- Add autocommand for git commit on save
-        local group = "DailyNotesGit"
-        vim.api.nvim_create_augroup(group, { clear = true })
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            group = group,
-            pattern = vim.fn.expand(opts.path) .. "**",
-            callback = function()
-                vim.fn.jobstart(
-                    "git -C "
-                        .. vim.fn.expand(opts.path)
-                        .. " add . && git -C "
-                        .. vim.fn.expand(opts.path)
-                        .. " commit -m \""
-                        .. os.date("%Y-%m-%d-%H-%M-%S")
-                        .. "\" && git -C "
-                        .. vim.fn.expand(opts.path)
-                        .. " push",
-                    {
-                        on_stdout = function(_, data)
-                            if data then
-                                vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
-                            end
-                        end,
-                        on_exit = function(_, code, _)
-                            if code == 0 then
-                                vim.notify("Push successful", vim.log.levels.INFO)
-                            else
-                                vim.notify("Push failed", vim.log.levels.ERROR)
-                            end
-                        end,
-                    }
-                )
-            end,
-        })
+        local path = vim.fn.expand(opts.path)
+        print("HIT")
+        print(path)
+        -- local group = "DailyNotesGit"
+        -- vim.api.nvim_create_augroup(group, { clear = true })
+        -- vim.api.nvim_create_autocmd("BufWritePost", {
+        --     group = group,
+        --     pattern = path .. "**",
+        --     callback = function()
+        --         vim.fn.jobstart(
+        --             "git add . && git commit -m " .. os.date("%Y-%m-%d-%H-%M-%S") .. " git pull --rebase && git push",
+        --             {
+        --                 cwd = path,
+        --                 on_stdout = function(_, data)
+        --                     if data then
+        --                         vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
+        --                     end
+        --                 end,
+        --                 on_exit = function(_, code, _)
+        --                     if code == 0 then
+        --                         vim.notify("Push successful", vim.log.levels.INFO)
+        --                     else
+        --                         vim.notify("Push failed", vim.log.levels.ERROR)
+        --                     end
+        --                 end,
+        --             }
+        --         )
+        --     end,
+        -- })
     end,
 }
