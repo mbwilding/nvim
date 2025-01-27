6f7c724748221a597877cdc6c526849fa0db493a
Matthew Wilding <mbwilding@gmail.com>
2025-01-28 00:09:05 +0800

    2025-01-28-00-09-05

--- lua/config/signs.lua
+++ lua/config/signs.lua
@@ -19,27 +19,3 @@ local signs = {
 for _, sign in ipairs(signs) do
     vim.fn.sign_define(sign[1], sign[2])
 end
-
--- vim.diagnostic.config({
---     signs = {
---         text = {
---             [vim.diagnostic.severity.ERROR] = "",
---             [vim.diagnostic.severity.WARN] = "",
---             [vim.diagnostic.severity.INFO] = "",
---             [vim.diagnostic.severity.HINT] = "",
---         },
---         -- Highlights
---         -- linehl = {
---         --     [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
---         --     [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
---         --     [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
---         --     [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
---         -- },
---         -- numhl = {
---         --     [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
---         --     [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
---         --     [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
---         --     [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
---         -- },
---     },
--- })
