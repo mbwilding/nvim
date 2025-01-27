b98275703852bbe57d3af586e6ce73aa54230d94
Matthew Wilding <mbwilding@gmail.com>
2025-01-28 00:15:47 +0800

    update

--- lua/config/signs.lua
+++ lua/config/signs.lua
@@ -19,3 +19,34 @@ local signs = {
 for _, sign in ipairs(signs) do
     vim.fn.sign_define(sign[1], sign[2])
 end
+
+-- vim.diagnostic.config({
+--     signs = {
+--         text = {
+--             [vim.diagnostic.severity.ERROR] = "",
+--             [vim.diagnostic.severity.WARN] = "",
+--             [vim.diagnostic.severity.INFO] = "",
+--             [vim.diagnostic.severity.HINT] = "",
+--         },
+--         -- Highlights
+--         texthl = {
+--             [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+--             [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+--             [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+--             [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+--             ["DapLogPoint"] = "DapLogPoint",
+--             ["DapStopped"] = "DapStopped",
+--             ["DapBreakpoint"] = "DapBreakpoint",
+--         },
+--         linehl = {
+--             ["DapLogPoint"] = "DapLogPoint",
+--             ["DapStopped"] = "DapStopped",
+--             ["DapBreakpoint"] = "DapBreakpoint",
+--         },
+--         numhl = {
+--             ["DapLogPoint"] = "DapLogPoint",
+--             ["DapStopped"] = "DapStopped",
+--             ["DapBreakpoint"] = "DapBreakpoint",
+--         },
+--     },
+-- })
