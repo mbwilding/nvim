5ee4a87664750f61754e8168ac2e5ffdc9a8da87
Matthew Wilding <mbwilding@gmail.com>
2025-01-29 00:45:41 +0800

    update

--- lua/plugins/gronk.lua
+++ lua/plugins/gronk.lua
@@ -3,13 +3,14 @@
 return {
     "mbwilding/gronk.nvim",
     lazy = false,
-    dev = false,
+    dev = true,
     priority = 1000,
     config = function()
         require("gronk").setup({
             transparent = false,
         })
 
-        vim.cmd([[colorscheme gronk-wilding]])
+        -- vim.cmd([[colorscheme gronk-wilding]])
+        vim.cmd([[colorscheme gronk-mono]])
     end,
 }
