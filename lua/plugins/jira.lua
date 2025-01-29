034b9e028f4a938cf2c51744dc653c3d355a6f62
Matthew Wilding <mbwilding@gmail.com>
2025-01-29 16:48:43 +0800

    update

--- lua/plugins/jira.lua
+++ lua/plugins/jira.lua
@@ -1,26 +1,22 @@
 return {
-    -- "kid-icarus/jira.nvim",
-    "mbwilding/jira.nvim",
-    -- lazy = true,
-    dev = false,
+    "kid-icarus/jira.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    opts = {
-        jira_api = {
-            domain = vim.env.JIRA_DOMAIN,
-            username = vim.env.JIRA_USER,
-            token = vim.env.JIRA_API_TOKEN,
+    keys = {
+        { "<leader>jv", "<cmd>Jira issue view<cr>", desc = "Jira: Issue view" },
+        { "<leader>jc", "<cmd>Jira issue create<cr>", desc = "Jira: Issue create" },
+        {
+            "<leader>jt",
+            function()
+                require("jira.pickers.snacks").transitions()
+            end,
+            desc = "Jira: Transitions",
         },
-        use_git_branch_issue_id = true,
-        git_trunk_branch = "main", -- The main branch of your project
-        git_branch_prefix = "feature/", -- The prefix for your feature branches
     },
-    config = function(_, opts)
-        require("jira").setup(opts)
-
-        vim.keymap.set("n", "<leader>jv", "<cmd>Jira issue view<cr>", { desc = "Jira: Issue view" })
-        vim.keymap.set("n", "<leader>jc", "<cmd>Jira issue create<cr>", { desc = "Jira: Issue create" })
-        vim.keymap.set("n", "<leader>jt", require("jira.pickers.snacks").transitions, { desc = "Jira: Transitions" })
-    end,
+    opts = {
+        use_git_branch_issue_id = true,
+        git_trunk_branch = "main",
+        git_branch_prefix = "feature/",
+    },
 }
