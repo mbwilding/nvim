a17e0b348ce8ac92e1bab74bf3ae164771dd2479
Matthew Wilding <mbwilding@gmail.com>
2025-02-27 22:31:47 +0800

    2025-02-27-22-31-47

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -45,21 +45,21 @@ return {
             modes = { insert = true, command = false, terminal = false },
 
             mappings = {
-                ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-                [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+                ["("] = { action = "open", pair = "()", neigh_pattern = "[^(]." },
+                [")"] = { action = "close", pair = "()", neigh_pattern = "[^)]." },
 
-                ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-                ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+                ["["] = { action = "open", pair = "[]", neigh_pattern = "[^[]." },
+                ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^]]." },
 
-                ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-                ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+                ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^{]." },
+                ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^}]." },
 
-                ["<"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-                [">"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+                ["<"] = { action = "open", pair = "{}", neigh_pattern = "[^<]." },
+                [">"] = { action = "close", pair = "{}", neigh_pattern = "[^>]." },
 
-                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '[^"].', register = { cr = false } },
+                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^'].", register = { cr = false } },
+                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^`].", register = { cr = false } },
             },
         })
 
