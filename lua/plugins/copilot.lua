34ddd5b5e2bf49d750b21d6f6ca49d3420ac070f
Matthew Wilding <mbwilding@gmail.com>
2025-03-21 09:54:34 +0800

    update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -11,7 +11,19 @@ return {
     },
     opts = {
         panel = {
-            enable = false,
+            enabled = true,
+            auto_refresh = false,
+            keymap = {
+                jump_prev = "[[",
+                jump_next = "]]",
+                accept = "<CR>",
+                refresh = "gr",
+                open = "<M-CR>"
+            },
+            layout = {
+                position = "bottom", -- | top | left | right | horizontal | vertical
+                ratio = 0.4
+            },
         },
         suggestion = {
             enabled = true,
@@ -19,24 +31,26 @@ return {
             hide_during_completion = true,
             debounce = 75,
             keymap = {
-                accept = "<C-y>",
+                accept = "<M-l>",
                 accept_word = false,
                 accept_line = false,
-                next = "<C-n>",
-                prev = "<C-p>",
-                dismiss = "<C-Esc>",
+                next = "<M-]>",
+                prev = "<M-[>",
+                dismiss = "<C-]>",
             },
         },
         filetypes = {
-            cvs = false,
-            gitcommit = true,
-            gitrebase = false,
+            yaml = true,
+            markdown = true,
             help = false,
+            gitcommit = false,
+            gitrebase = false,
             hgcommit = false,
-            markdown = false,
             svn = false,
-            yaml = false,
+            cvs = false,
             ["."] = false,
         },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
     },
 }
