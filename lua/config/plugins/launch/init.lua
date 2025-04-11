58a65a03e259a56e77a3ea2168dd50524865b6d1
Matthew Wilding <mbwilding@gmail.com>
2025-04-11 22:29:05 +0800

    2025-04-11-22-29-05

--- lua/config/plugins/launch/init.lua
+++
@@ -1,8 +0,0 @@
--- TODO
--- Search all files (rg?) and from file extensions, do patterns, rather than defining patterns that search all files anyway
-
-local utils = require("config/plugins/launch/utils")
-
-vim.keymap.set("n", "<leader>0", function()
-    utils.select_project(utils.merge())
-end)
