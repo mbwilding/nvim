9614b77a43a0fb4e6c1f998b41bb1df81caddb19
Matthew Wilding <mbwilding@gmail.com>
2025-04-12 12:16:21 +0800

    update

--- lua/plugins/launcher.lua
+++ lua/plugins/launcher.lua
@@ -2,14 +2,8 @@ return {
     "mbwilding/launcher.nvim",
     dev = true,
     config = function()
-        local utils = require("launcher.utils")
-
         vim.keymap.set("n", "<leader>0", function()
-            local files = utils.get_relevant_files()
-
-            for _, file in ipairs(files) do
-                print(file)
-            end
+            require("launcher").run()
         end, { desc = "Launcher" })
     end,
 }
