100f72fd040c31153ade8fde06228cc8ecf7dce9
Matthew Wilding <mbwilding@gmail.com>
2025-04-13 04:48:48 +0800

    2025-04-13-04-48-48

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -2,5 +2,9 @@ return {
     "andrewferrier/debugprint.nvim",
     opts = {
         highlight_lines = false,
+        print_tag = "DEBUG",
+        display_location = false,
+        display_counter = true,
+        display_snippet = false,
     },
 }
