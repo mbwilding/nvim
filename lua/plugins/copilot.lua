942ab9b6f349edabd2d0b96f69b6d133c9a6487c
Matthew Wilding <mbwilding@gmail.com>
2025-04-15 14:07:23 +0800

    update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -32,7 +32,7 @@ return {
             hide_during_completion = true,
             debounce = 75,
             keymap = {
-                accept = "<M-l>",
+                accept = "<M-Enter>",
                 accept_word = false,
                 accept_line = false,
                 next = "<M-]>",
