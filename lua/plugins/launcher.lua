40f209af17dfc7552e741f9016937ceab05c3cc9
Matthew Wilding <mbwilding@gmail.com>
2025-04-29 01:40:29 +0800

    update

--- lua/plugins/launcher.lua
+++ lua/plugins/launcher.lua
@@ -1,7 +1,9 @@
 return {
     "mbwilding/launcher.nvim",
-    -- dev = true,
+    dev = true,
     config = function()
+        vim.g.unreal_engine_path = os.getenv("HOME") .. "/dev/UnrealEngine"
+
         vim.keymap.set("n", "<leader><esc>", function()
             require("launcher").run()
         end, { desc = "Launcher" })
