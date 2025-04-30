74cb50f80a39d6348593e56e8eb8282bd4a05915
Matthew Wilding <mbwilding@gmail.com>
2025-04-30 14:53:23 +0800

    update

--- lua/plugins/unrealengine.lua
+++ lua/plugins/unrealengine.lua
@@ -21,8 +21,10 @@ return {
         local engine_path
         if jit.os == "Linux" then
             engine_path = os.getenv("HOME") .. "/dev/UnrealEngine"
-        else
+        elseif jit.os == "OSX" then
             engine_path = "/Volumes/SamsungT7/UnrealEngine"
+        elseif jit.os == "Windows" then
+            engine_path = "D:\\UnrealEngine"
         end
 
         require("unrealengine").setup({
