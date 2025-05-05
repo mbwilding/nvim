d7de094813f24491337fee4e6f74033b01464550
Matthew Wilding <mbwilding@gmail.com>
2025-05-05 19:50:53 +0800

    update

--- lua/plugins/launcher.lua
+++ lua/plugins/launcher.lua
@@ -2,20 +2,18 @@ return {
     "mbwilding/launcher.nvim",
     dev = true,
     config = function()
-        if jit.os == "Linux" then
-            vim.g.unreal_engine_path = os.getenv("HOME") .. "/dev/UnrealEngine"
-        else
-            vim.g.unreal_engine_path = "/Volumes/SamsungT7/UnrealEngine"
-        end
+        local opts = {
+            custom_dir = vim.fn.stdpath("config") .. "/lua/launcher"
+        }
 
         local prefix = "Launcher: "
 
         vim.keymap.set("n", "<leader>lp", function()
-            require("launcher").file()
+            require("launcher").file(opts)
         end, { desc = prefix .. "Picker" })
 
         vim.keymap.set("n", "<leader>lr", function()
-            require("launcher").rerun()
+            require("launcher").rerun(opts)
         end, { desc = prefix .. "Rerun" })
     end,
 }
