90c0ea9796a669780ea809a8b6e7b5f5c3a0bcbf
Matthew Wilding <mbwilding@gmail.com>
2025-05-11 23:25:04 +0800

    update

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,53 +1,17 @@
 local servers = {
-    zls = {},
-    clangd = {},
-    gopls = {},
-    html = {},
+    -- roslyn_ls = {}, -- roslyn.nvim
     azure_pipelines_ls = {},
+    clangd = {},
     docker_compose_language_service = {},
     dockerls = {},
-    vuels = {},
-    taplo = {},
-    sqls = {},
-    pylsp = {},
+    gopls = {},
+    html = {},
     jsonls = {},
-    roslyn_ls = {
-        settings = {
-            ["csharp|background_analysis"] = {
-                dotnet_analyzer_diagnostics_scope = "fullSolution",
-                dotnet_compiler_diagnostics_scope = "fullSolution",
-            },
-            ["csharp|code_lens"] = {
-                dotnet_enable_references_code_lens = true,
-                dotnet_enable_tests_code_lens = true,
-            },
-            ["csharp|completion"] = {
-                dotnet_provide_regex_completions = true,
-                dotnet_show_completion_items_from_unimported_namespaces = true,
-                dotnet_show_name_completion_suggestions = true,
-            },
-            ["csharp|inlay_hints"] = {
-                csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                csharp_enable_inlay_hints_for_implicit_variable_types = true,
-                csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-                csharp_enable_inlay_hints_for_types = true,
-                dotnet_enable_inlay_hints_for_indexer_parameters = true,
-                dotnet_enable_inlay_hints_for_literal_parameters = true,
-                dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-                dotnet_enable_inlay_hints_for_other_parameters = true,
-                dotnet_enable_inlay_hints_for_parameters = true,
-                dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-                dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-                dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-            },
-            ["csharp|symbol_search"] = {
-                dotnet_search_reference_assemblies = true,
-            },
-            ["csharp|formatting"] = {
-                dotnet_organize_imports_on_format = true,
-            },
-        },
-    },
+    pylsp = {},
+    sqls = {},
+    taplo = {},
+    vuels = {},
+    zls = {},
     lua_ls = {
         settings = {
             Lua = {
