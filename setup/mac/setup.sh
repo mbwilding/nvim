015df9720e43b635064b18692f41ce2464dc0d7c
Matthew Wilding <mbwilding@gmail.com>
2025-05-11 23:36:33 +0800

    update

--- setup/mac/setup.sh
+++ setup/mac/setup.sh
@@ -3,7 +3,13 @@
 # HomeBrew
 brew install neovim --HEAD
 brew tap oven-sh/bun
-brew install bun jq curl wget node python go ripgrep fzf rustup lua-language-server taplo python-lsp-server yaml-language-server tailwindcss-language-server powershell bash-language-server zls llvm gopls
+brew install bun jq curl wget node python go ripgrep fzf rustup powershell llvm
+
+## Language Servers
+brew install lua-language-server taplo python-lsp-server yaml-language-server tailwindcss-language-server bash-language-server zls gopls
+
+## Linters
+brew install actionlint clang-format eslint_d jsonlint markdownlint-cli prettierd shfmt yamllint
 
 # Go
 go install github.com/sqls-server/sqls@latest
@@ -13,6 +19,7 @@ bun install --global @microsoft/compose-language-service
 bun install --global azure-pipelines-language-server
 bun install --global dockerfile-language-server-nodejs
 bun install --global vue-language-server
+bun install --global xml-formatter
 bun install --global vscode-langservers-extracted
 # - vscode-css-language-server
 # - vscode-eslint-language-server
