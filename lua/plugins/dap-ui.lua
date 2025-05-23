df125beb1a8eacd2ec313ad0777bac86ace456fa
Matthew Wilding <mbwilding@gmail.com>
2025-05-24 02:23:01 +0800

    update

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -1,9 +1,6 @@
 return {
     "rcarriga/nvim-dap-ui",
     opts = {
-        -- Set icons to characters that are more likely to work in every terminal.
-        --    Feel free to remove or use ones that you like more! :)
-        --    Don't feel like these are good choices.
         icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
         controls = {
             icons = {
@@ -25,7 +22,7 @@ return {
 
         dapui.setup(opts)
 
-        vim.keymap.set("n", "<leader>dap", dapui.toggle, { desc = "Debug: See last session result." })
+        vim.keymap.set("n", "<leader>dap", dapui.toggle, { desc = "Debug: See last session result" })
 
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
