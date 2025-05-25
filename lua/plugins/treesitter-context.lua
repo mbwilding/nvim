cd31127ceef6df5f34494a2f5b3615db49a1bdbd
Matthew Wilding <mbwilding@gmail.com>
2025-05-26 00:31:46 +0800

    update

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,26 @@
+local toggled = false
+
+return {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = {
+        {
+            "nvim-treesitter/nvim-treesitter",
+            branch = "main",
+        },
+    },
+    keys = {
+        {
+            "<leader>ix",
+            function()
+                require("treesitter-context").toggle()
+                toggled = not toggled
+                vim.notify("Treesitter Context: " .. tostring(toggled))
+            end,
+            desc = "Treesitter: Context Rows",
+        },
+    },
+    opts = {
+        enable = false,
+        mode = "topline", -- Also: cursor
+    },
+}
