324ad3987d453d8b428d933db3b1da60c265f81e
Matthew Wilding <mbwilding@gmail.com>
2025-05-26 23:46:26 +0800

    update

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,67 +4,47 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
-    keys = {
-        {
-            "<leader>aia",
-            "<CMD>CodeCompanionActions<CR>",
-            desc = "CodeCompanion: Actions",
-            mode = { "n", "v" },
-        },
-        {
-            "<leader>aic",
-            "<CMD>CodeCompanionChat Toggle<CR>",
-            desc = "CodeCompanion: Chat",
-            mode = { "n", "v" },
-        },
-    },
     opts = {
-        system_prompt = function(opts)
-            return "Only display the code if it is related to coding or simply answer the question directly, without unnecessary talking."
-        end,
-        adapters = {
-            openai = function()
-                return require("codecompanion.adapters").extend("openai", {
-                    schema = {
-                        model = {
-                            default = "o3-mini",
-                        },
-                    },
-                })
-            end,
-            copilot = function()
-                return require("codecompanion.adapters").extend("copilot", {
-                    schema = {
-                        model = {
-                            default = "o3-mini",
-                        },
-                    },
-                })
-            end,
-        },
         strategies = {
             chat = {
                 adapter = "openai",
             },
             inline = {
-                adapter = "copilot",
-                language = "English",
-                -- layout = "vertical",
-                keymaps = {
-                    accept_change = {
-                        modes = { n = "ga" },
-                        description = "Accept the suggested change",
-                    },
-                    reject_change = {
-                        modes = { n = "gr" },
-                        description = "Reject the suggested change",
-                    },
-                },
+                adapter = "openai",
             },
         },
     },
     config = function(_, opts)
         require("codecompanion").setup(opts)
-        vim.cmd([[cab cc CodeCompanion]])
+
+        vim.keymap.set(
+            { "n", "v" },
+            "<leader>aia",
+            "<CMD>CodeCompanionActions<CR>",
+            { desc = "CodeCompanion: Actions" }
+        )
+        vim.keymap.set(
+            { "n", "v" },
+            "<leader>aic",
+            "<CMD>CodeCompanionChat Toggle<CR>",
+            { desc = "CodeCompanion: Chat" }
+        )
+
+        local function codecompanion_prompt(command_prefix)
+            vim.ui.input({ prompt = "AI: " }, function(input)
+                if input and input ~= "" then
+                    local prefix = command_prefix and (command_prefix .. " ") or ""
+                    vim.cmd("CodeCompanion " .. prefix .. input)
+                end
+            end)
+        end
+
+        vim.keymap.set("n", "<leader>aii", function()
+            codecompanion_prompt("#buffer")
+        end, { desc = "CodeCompanion: Buffer" })
+
+        vim.keymap.set("v", "<leader>aii", function()
+            codecompanion_prompt()
+        end, { desc = "CodeCompanion: Selection" })
     end,
 }
