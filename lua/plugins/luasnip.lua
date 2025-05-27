8db2ace5bb5ee901c185f8019f9e616c876d3b12
Matthew Wilding <mbwilding@gmail.com>
2025-05-27 09:20:54 +0800

    update

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,6 +4,7 @@ return {
     "L3MON4D3/LuaSnip",
     lazy = true,
     version = "v2.*",
+    build = "make install_jsregexp",
     opts = {},
     config = function(_, opts)
         local luasnip = require("luasnip")
