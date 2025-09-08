7124b3f3017a13a94fc5ef9d665df83c3b5a43ea
Matthew Wilding <mbwilding@gmail.com>
2025-09-08 23:25:42 +0800

    update

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -6,6 +6,9 @@ if vim.g.neovide then
     if vim.fn.has("mac") == 1 then
         vim.o.guifont = font_suffix .. "18"
         vim.g.neovide_refresh_rate = 60
+    elseif vim.fn.hostname() == "nona" then
+        vim.o.guifont = font_suffix .. "26"
+        vim.g.neovide_refresh_rate = 90
     else
         vim.o.guifont = font_suffix .. "17" -- :w-1
         vim.g.neovide_refresh_rate = 120
