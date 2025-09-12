7c610c2a764693b24e57ac74fca31ae6568cd85b
Matthew Wilding <mbwilding@gmail.com>
2025-09-12 09:41:33 +0800

    Fixed

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -1,5 +1,13 @@
 ; extends
 (block_mapping_pair
+  key: (flow_node) @_bash
+  (#any-of? @_bash "bash")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
   key: (flow_node) @_pwsh
   (#any-of? @_pwsh "pwsh")
   value: (block_node
