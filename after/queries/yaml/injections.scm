ca6ef00bbde36d33f77ce0ec4c92d8f033c84486
Matthew Wilding <mbwilding@gmail.com>
2025-10-05 23:14:38 +0800

    Update injections

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -1,24 +1,51 @@
 ; extends
+
+; --- Inject Bash and PowerShell for common CI pipeline fields ---
+
+; Azure DevOps, GitHub Actions, GitLab CI (sh, bash, script, run, before_script, after_script as block_scalar)
 (block_mapping_pair
-  key: (flow_node) @_bash
-  (#any-of? @_bash "bash")
-  value: (block_node
-    (block_scalar) @injection.content
-    (#set! injection.language "bash")
-    (#offset! @injection.content 0 1 0 0)))
+  key: (flow_node) @key
+    (#any-of? @key "script" "run" "bash" "sh" "before_script" "after_script" "inlineScript")
+  value: (block_node (block_scalar) @injection.content)
+  (#set! injection.language "bash")
+  (#offset! @injection.content 0 1 0 0)
+)
+
+; Azure DevOps PowerShell
+(block_mapping_pair
+  key: (flow_node) @key
+    (#any-of? @key "pwsh" "powershell" "inlineScript")
+  value: (block_node (block_scalar) @injection.content)
+  (#set! injection.language "powershell")
+  (#offset! @injection.content 0 1 0 0)
+)
+
+; --- Support inline and plain string scenarios ---
+(block_mapping_pair
+  key: (flow_node) @key
+    (#any-of? @key "script" "run" "bash" "sh" "before_script" "after_script" "inlineScript")
+  value: (flow_node (plain_scalar) @injection.content)
+  (#set! injection.language "bash")
+)
 
 (block_mapping_pair
-  key: (flow_node) @_pwsh
-  (#any-of? @_pwsh "pwsh")
-  value: (block_node
-    (block_scalar) @injection.content
-    (#set! injection.language "powershell")
-    (#offset! @injection.content 0 1 0 0)))
+  key: (flow_node) @key
+    (#any-of? @key "pwsh" "powershell" "inlineScript")
+  value: (flow_node (plain_scalar) @injection.content)
+  (#set! injection.language "powershell")
+)
 
-(block_mapping_pair
-  key: (flow_node) @_pwsh
-  (#any-of? @_pwsh "inlineScript")
-  value: (block_node
-    (block_scalar) @injection.content
-    (#set! injection.language "powershell")
-    (#offset! @injection.content 0 1 0 0)))
+; --- Variable/Template Interpolation ---
+; Handles {{ }} and similar for gotmpl-like highlighting
+(block_scalar) @injection.content
+  (#match? @injection.content "\\{\\{\\s*[A-Za-z_.][A-Za-z0-9_.]*\\s*\\}\}")
+  (#set! injection.language "gotmpl")
+
+(block_scalar) @injection.content
+  (#match? @injection.content "\\$\\{\\{[^}]+\\}\}")
+  (#set! injection.language "gotmpl")
+
+; Bash variable style
+(block_scalar) @injection.content
+  (#match? @injection.content "\\$[A-Za-z_][A-Za-z0-9_]*")
+  (#set! injection.language "bash")
