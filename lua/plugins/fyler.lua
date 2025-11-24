4ed3463422591db1e882c3a452a714eecf8d9213
Matthew Wilding <mbwilding@gmail.com>
2025-11-24 12:53:48 +0800

    Update

---
+++ lua/plugins/fyler.lua
@@ -0,0 +1,13 @@
+return {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = { "nvim-mini/mini.icons" },
+    branch = "stable",
+    keys = {
+        {
+            "<leader>e",
+            "<CMD>Fyler<CR>",
+            desc = "Fyler",
+        },
+    },
+    opts = {},
+}
