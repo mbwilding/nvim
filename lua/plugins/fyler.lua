260bee0193688a1c2718b5109a457a4999212790
Matthew Wilding <mbwilding@gmail.com>
2025-12-07 21:58:12 +0800

    update

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -9,5 +9,11 @@ return {
             desc = "Fyler",
         },
     },
-    opts = {},
+    opts = {
+        hooks = {
+            on_rename = function(src_path, destination_path)
+                Snacks.rename.on_rename_file(src_path, destination_path)
+            end,
+        },
+    },
 }
