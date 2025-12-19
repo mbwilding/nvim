77fcca3253e4db81563f943be72022dba64cf26b
Matthew Wilding <mbwilding@gmail.com>
2025-12-19 22:49:38 +0800

    update

--- lua/plugins/lsp-lens.lua
+++ lua/plugins/lsp-lens.lua
@@ -29,12 +29,12 @@ return {
                     end
                 end,
                 git_authors = false,
-                git_authors = function(latest_author, count)
-                    if latest_author == "Not Committed Yet" then
-                        latest_author = "uncommitted"
-                    end
-                    return "  " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-                end,
+                -- git_authors = function(latest_author, count)
+                --     if latest_author == "Not Committed Yet" then
+                --         latest_author = "uncommitted"
+                --     end
+                --     return "  " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+                -- end,
             },
             ignore_filetype = {
                 "prisma",
