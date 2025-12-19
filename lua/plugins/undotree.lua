2de1b09908143eb9fdf388acf64604f035b9c308
Matthew Wilding <mbwilding@gmail.com>
2025-12-19 18:52:23 +0800

    update

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,11 +1,29 @@
 return {
-    "mbbill/undotree",
-    lazy = true,
+    "jiaoshijie/undotree",
     keys = {
         {
             "<leader>u",
-            "<CMD>UndotreeToggle<CR>",
-            desc = "UndoTree: Toggle",
+            function()
+                require("undotree").toggle()
+            end,
+            "UndoTree",
+        },
+    },
+    ---@module "undotree.collector"
+    ---@type UndoTreeCollector.Opts
+    opts = {
+        float_diff = false,
+        layout = "left_bottom",
+        position = "left",
+        keymaps = {
+            j = "move_next",
+            k = "move_prev",
+            gj = "move2parent",
+            J = "move_change_next",
+            K = "move_change_prev",
+            ["<cr>"] = "action_enter",
+            p = "enter_diffbuf",
+            q = "quit",
         },
     },
 }
