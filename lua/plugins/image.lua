5d7342c496ee7d7d1258c3c73313e6b653e36f7a
Matthew Wilding <mbwilding@gmail.com>
2025-12-20 04:21:47 +0800

    update

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,7 +1,7 @@
 return {
     "3rd/image.nvim",
     -- dependencies = { "luarocks.nvim" },
-    enabled = false,
+    enabled = true,
     opts = {
         backend = "kitty",
         processor = "magick_cli",
