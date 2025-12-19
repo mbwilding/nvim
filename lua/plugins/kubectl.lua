f62a5dc4d1d79588f6f19f4170178d98c075ec6e
Matthew Wilding <mbwilding@gmail.com>
2025-12-19 13:10:59 +0800

    Update kubectl

--- lua/plugins/kubectl.lua
+++ lua/plugins/kubectl.lua
@@ -1,25 +1,22 @@
 return {
-    {
-        "ramilito/kubectl.nvim",
-        version = "2.*",
-        build = "make build_go && cargo build --release --features kube/socks5",
-        dependencies = "saghen/blink.download",
-        keys = {
-            {
-                "<leader>kc",
-                function()
-                    require("kubectl").toggle()
-                end,
-                desc = "Kubectl",
-            },
+    "ramilito/kubectl.nvim",
+    version = "2.*",
+    dependencies = "saghen/blink.download",
+    keys = {
+        {
+            "<leader>kc",
+            function()
+                require("kubectl").toggle()
+            end,
+            desc = "Kubectl",
         },
-        opts = {
-            terminal_cmd = "ghostty -e",
-            logs = {
-                prefix = true,
-                timestamps = true,
-                since = "5m",
-            },
+    },
+    opts = {
+        terminal_cmd = "ghostty -e",
+        logs = {
+            prefix = true,
+            timestamps = true,
+            since = "5m",
         },
     },
 }
