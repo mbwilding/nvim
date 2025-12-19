f5153937cfbb1b644c75bfa7401c3a481c2ac031
Matthew Wilding <mbwilding@gmail.com>
2025-12-20 04:49:58 +0800

    update

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -1,6 +1,6 @@
 ; extends
 
-; Azure DevOps, GitHub Actions, GitLab CI
+; Bash
 (block_mapping_pair
   key: (flow_node) @key
     (#any-of? @key "script" "run" "bash" "sh" "before_script" "after_script" "inlineScript")
@@ -9,21 +9,20 @@
   (#offset! @injection.content 0 1 0 0)
 )
 
-; Azure DevOps PowerShell
+(block_mapping_pair
+  key: (flow_node) @key
+    (#any-of? @key "script" "run" "bash" "sh" "before_script" "after_script" "inlineScript")
+  value: (flow_node (plain_scalar) @injection.content)
+  (#set! injection.language "bash")
+)
+
+; Powershell
 (block_mapping_pair
   key: (flow_node) @key
     (#any-of? @key "pwsh" "powershell" "inlineScript")
   value: (block_node (block_scalar) @injection.content)
   (#set! injection.language "powershell")
   (#offset! @injection.content 0 1 0 0)
-)
-
-; --- Support inline and plain string scenarios ---
-(block_mapping_pair
-  key: (flow_node) @key
-    (#any-of? @key "script" "run" "bash" "sh" "before_script" "after_script" "inlineScript")
-  value: (flow_node (plain_scalar) @injection.content)
-  (#set! injection.language "bash")
 )
 
 (block_mapping_pair
