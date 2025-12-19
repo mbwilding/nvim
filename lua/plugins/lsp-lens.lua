8d7aaec098e39aa093dada51ef357910b9bc5049
Matthew Wilding <mbwilding@gmail.com>
2025-12-19 23:25:15 +0800

    update

--- lua/plugins/lsp-lens.lua
+++ lua/plugins/lsp-lens.lua
@@ -2,43 +2,48 @@ return {
     "VidocqH/lsp-lens.nvim",
     lazy = true,
     event = "LspAttach",
-    config = function()
-        local SymbolKind = vim.lsp.protocol.SymbolKind
-
-        require("lsp-lens").setup({
-            enable = true,
-            include_declaration = false,
-            sections = {
-                definition = function(count)
-                    if count > 1 then
-                        return count .. " definitions"
-                    end
-                end,
-                references = function(count)
-                    if count > 1 then
-                        return count .. " usages"
-                    end
-                end,
-                implements = function(count)
-                    if count > 1 then
-                        return count .. " implementations"
-                    end
-                end,
-                git_authors = false,
-                -- git_authors = function(latest_author, count)
-                --     if latest_author == "Not Committed Yet" then
-                --         latest_author = "uncommitted"
-                --     end
-                --     return "  " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
-                -- end,
-            },
-            ignore_filetype = {
-                "prisma",
-            },
-            target_symbol_kinds = { SymbolKind.Function, SymbolKind.Method, SymbolKind.Interface },
-            wrapper_symbol_kinds = { SymbolKind.Class, SymbolKind.Struct },
-        })
-
-        vim.keymap.set("n", "<leader>iu", "<cmd>LspLensToggle<CR>", { desc = "LSP Lens: Toggle" })
-    end,
+    keys = {
+        {
+            "<leader>ui",
+            "<CMD>LspLensToggle<CR>",
+            desc = "Launcher: File",
+        },
+    },
+    opts = {
+        enable = true,
+        include_declaration = false,
+        sections = {
+            definition = function(count)
+                if count > 1 then
+                    return count .. " definitions"
+                end
+            end,
+            references = function(count)
+                if count > 1 then
+                    return count .. " usages"
+                end
+            end,
+            implements = function(count)
+                if count > 1 then
+                    return count .. " implementations"
+                end
+            end,
+            git_authors = false,
+            -- git_authors = function(latest_author, count)
+            --     if latest_author == "Not Committed Yet" then
+            --         latest_author = "uncommitted"
+            --     end
+            --     return "  " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+            -- end,
+        },
+        ignore_filetype = {
+            "prisma",
+        },
+        target_symbol_kinds = {
+            vim.lsp.protocol.SymbolKind.Function,
+            vim.lsp.protocol.SymbolKind.Method,
+            vim.lsp.protocol.SymbolKind.Interface,
+        },
+        wrapper_symbol_kinds = { vim.lsp.protocol.SymbolKind.Class, vim.lsp.protocol.SymbolKind.Struct },
+    },
 }
