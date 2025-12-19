0a6578eb8442cd4b2907a023c553be3d0cb7d111
Matthew Wilding <mbwilding@gmail.com>
2025-12-20 05:05:47 +0800

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,5 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            branch = "main",
-        },
-    },
     lazy = false,
     branch = "main",
     build = ":TSUpdate",
