3cd7a4a58165f31d5cc14eeb32316bd13f00cb69
Matthew Wilding <mbwilding@gmail.com>
2025-12-21 04:57:15 +0800

    update

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,12 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
         "refractalize/oil-git-status.nvim",
-        "JezerM/oil-lsp-diagnostics.nvim",
+        {
+            -- "JezerM/oil-lsp-diagnostics.nvim",
+            "mbwilding/oil-lsp-diagnostics.nvim",
+            branch = "feat/diagnostic-count"
+            -- dev = true,
+        },
     },
     keys = {
         {
