d6aea7ea479a3a67cf58d856f99b508f5f8bfd49
Matthew Wilding <mbwilding@gmail.com>
2025-12-21 16:01:42 +0800

    update

--- lua/plugins/tail.lua
+++ lua/plugins/tail.lua
@@ -1,3 +1,10 @@
 return {
     "thgrass/tail.nvim",
+    opts = {
+        timestamps = true,
+        -- customise the format (see `:help os.date`)
+        -- timestamp_format = "%Y-%m-%d %H:%M:%S",
+        timestamp_format = "%H:%M:%S",
+        timestamp_hl = "LspInlayHint",
+    },
 }
