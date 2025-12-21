d6cc3d497c2b0106b1006941580244e21062ab35
Matthew Wilding <mbwilding@gmail.com>
2025-12-21 16:22:04 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     import = "plugins",
     change_detection = {
+        enabled = false,
         notify = false,
     },
     profiling = {
