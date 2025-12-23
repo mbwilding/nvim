6d0f31b0ab2ad9d0bfe9722184e978bf1a8385cc
Matthew Wilding <mbwilding@gmail.com>
2025-12-24 02:20:33 +0800

    update

--- setup/linux/setup_arch.sh
+++ setup/linux/setup_arch.sh
@@ -7,7 +7,7 @@ shared="$(dirname "$(readlink -f "$0")")/../shared"
 paru -Sy --noconfirm --skipreview --needed \
     powershell-bin bun-bin neovim-nightly-bin openssh gnupg wget jq fuse make grep ripgrep cmake unzip go python3 ruby rustup nodejs clang luarocks tree-sitter-cli \
     ruff zls vue-language-server taplo-cli sqls pyright vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman roslyn-ls \
-    netcoredbg delve python-debugpy codelldb-bin vscode-js-debug-bin \
+    netcoredbg delve python-debugpy codelldb-bin vscode-js-debug-bin vscode-bash-debug \
     eslint_d prettierd yamllint python-cfn-lint \
     markdownlint-cli2
 
