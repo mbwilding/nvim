60a6053d71a2603c3403d7716ed1035b8536b9ec
Matthew Wilding <mbwilding@gmail.com>
2025-12-23 17:59:31 +0800

    update

--- setup/linux/setup_arch.sh
+++ setup/linux/setup_arch.sh
@@ -6,7 +6,7 @@ shared="$(dirname "$(readlink -f "$0")")/../shared"
 
 paru -Sy --noconfirm --skipreview --needed \
     powershell-bin bun-bin neovim-nightly-bin openssh gnupg wget jq fuse make grep ripgrep cmake unzip go python3 ruby rustup nodejs clang luarocks tree-sitter-cli \
-    zls vue-language-server taplo-cli sqls python-lsp-server vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman \
+    zls vue-language-server taplo-cli sqls python-lsp-server vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman roslyn-ls \
     netcoredbg delve python-debugpy \
     eslint_d prettierd python-pylint yamllint python-cfn-lint \
     ruff markdownlint-cli2
@@ -15,8 +15,8 @@ rustup default stable
 rustup component add rust-analyzer
 go install golang.org/x/tools/cmd/goimports@latest &
 bun install --global @typescript/native-preview &
-# bash ${shared}/netcoredbg.sh &
 bash ${shared}/codelldb.sh &
 bash ${shared}/js-debug.sh &
-bash ${shared}/roslyn.sh &
+# bash ${shared}/roslyn.sh &
+# bash ${shared}/netcoredbg.sh &
 wait
