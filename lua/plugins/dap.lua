019c4ce1b68d6444bdd6330d75247a682c426f03
Matthew Wilding <mbwilding@gmail.com>
2025-12-26 03:35:56 +0800

    update

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -55,6 +55,79 @@ return {
                 virt_text_win_col = 80, -- position the virtual text at a fixed window column (starting from the first text column)
             },
         },
+        {
+            "rcarriga/nvim-dap-ui",
+            opts = {
+                icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+                controls = {
+                    icons = {
+                        pause = "",
+                        play = "",
+                        step_into = "",
+                        step_over = "",
+                        step_out = "",
+                        step_back = "",
+                        run_last = "",
+                        terminate = "",
+                        disconnect = "",
+                    },
+                },
+                layouts = {
+                    {
+                        -- You can change the order of elements in the sidebar
+                        elements = {
+                            -- Provide IDs as strings or tables with "id" and "size" keys
+                            {
+                                id = "scopes",
+                                size = 0.25, -- Can be float or integer > 1
+                            },
+                            { id = "breakpoints", size = 0.25 },
+                            { id = "stacks", size = 0.25 },
+                            { id = "watches", size = 0.25 },
+                        },
+                        size = 0.3,
+                        position = "right", -- Can be "left" or "right"
+                    },
+                    {
+                        elements = {
+                            "repl",
+                            "console",
+                        },
+                        size = 10,
+                        position = "bottom", -- Can be "bottom" or "top"
+                    },
+                },
+            },
+            config = function(_, opts)
+                local dap = require("dap")
+                local dapui = require("dapui")
+
+                dapui.setup(opts)
+
+                vim.keymap.set("n", "<leader>dap", dapui.toggle, { desc = "Debug: Toggle UI" })
+
+                dap.listeners.before.attach.dapui_config = function()
+                    dapui.open()
+                end
+
+                dap.listeners.before.launch.dapui_config = function()
+                    dapui.open()
+                end
+
+                dap.listeners.after.event_initialized.dapui_config = function()
+                    dapui.open()
+                    require("dap-powershell").correct_repl_colors()
+                end
+
+                dap.listeners.before.event_terminated.dapui_config = function()
+                    dapui.close()
+                end
+
+                dap.listeners.before.event_exited.dapui_config = function()
+                    dapui.close()
+                end
+            end,
+        },
     },
     config = function()
         local dap = require("dap")
