90afece3b0055a3ea507723cdaae11e2612989ce
Matthew Wilding <mbwilding@gmail.com>
2025-12-29 02:05:39 +0800

    update

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,7 +4,6 @@ local modules = {
     "keymaps",
     "autocmds",
     "cmds",
-    "filetypes",
     "platform",
     "neovide",
     "lazy",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,38 +4,56 @@ return {
     branch = "main",
     build = ":TSUpdate",
     config = function()
+        -- Custom registrations
+        vim.filetype.add({
+            extension = {
+                csproj = "xml",
+                esproj = "xml",
+                keymap = "c",
+                mdx = "markdown",
+                uproject = "json",
+                wsdl = "xml",
+            },
+        })
+
+        -- Treesitter directory
+        local treesitter_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/"
+
         -- Collect all available parsers
-        local queries_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/runtime/queries"
-        local file_types = {}
-        for name, type in vim.fs.dir(queries_dir) do
+        local parsers = {}
+        for name, type in vim.fs.dir(treesitter_dir .. "runtime/queries") do
             if type == "directory" then
-                table.insert(file_types, name)
+                table.insert(parsers, name)
             end
         end
 
         -- Install file type parsers
-        require("nvim-treesitter").install(file_types)
+        require("nvim-treesitter").install(parsers)
 
-        -- Additional file types
-        local file_types_extra = {
-            "cs"
-        }
+        -- Register known file types
+        dofile(treesitter_dir .. "plugin/filetypes.lua")
 
-        for _, file_type in ipairs(file_types_extra) do
-            table.insert(file_types, file_type)
-        end
+        -- Get file types
+        local file_types = vim.iter(parsers)
+          :map(function(parser)
+            return vim.treesitter.language.get_filetypes(parser)
+          end)
+          :flatten()
+          :totable()
 
-        -- Automatically activate
+        -- Auto-run
         vim.api.nvim_create_autocmd("FileType", {
             pattern = file_types,
-            callback = function()
+            callback = function(args)
                 -- Highlights
                 vim.treesitter.start()
+
                 -- Folds
                 vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
                 vim.wo[0][0].foldmethod = "expr"
+
                 -- Indentation
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                vim.bo[args.buf].indentexpr = "v:lua.require\"nvim-treesitter\".indentexpr()"
             end,
         })
     end,
