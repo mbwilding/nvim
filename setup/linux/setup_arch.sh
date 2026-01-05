a70f94ef419861785cc01f88410ebf952b5c5fd7
Matthew Wilding <mbwilding@gmail.com>
2026-01-05 18:55:59 +0800

    update

--- setup/linux/setup_arch.sh
+++ setup/linux/setup_arch.sh
@@ -6,16 +6,14 @@ shared="$(dirname "$(readlink -f "$0")")/../shared"
 
 paru -Sy --noconfirm --skipreview --needed \
     powershell-bin bun neovim-git openssh gnupg wget jq fuse make grep ripgrep cmake unzip go python3 ruby rustup nodejs clang luarocks tree-sitter-cli \
-    ruff zls vue-language-server taplo-cli sqls pyright vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman roslyn-ls ada_language_server-bin \
+    zls vue-language-server taplo-cli sqls pyright vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman roslyn-ls ada_language_server-bin \
     netcoredbg delve python-debugpy codelldb-bin vscode-js-debug-bin vscode-bash-debug bashdb \
-    eslint_d prettierd yamllint python-cfn-lint \
-    markdownlint-cli2
+    ruff eslint_d prettierd yamllint python-cfn-lint markdownlint-cli2
 
 rustup default stable
 rustup component add rust-analyzer
 
 go install golang.org/x/tools/cmd/goimports@latest &
-
 bun install --global @typescript/native-preview &
 
 wait
