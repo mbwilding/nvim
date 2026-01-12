1194ec21596de106f9eeecc86d861a4f2169031f
Matthew Wilding <mbwilding@gmail.com>
2026-01-12 22:14:58 +0800

    php lsp

--- setup/linux/setup_arch.sh
+++ setup/linux/setup_arch.sh
@@ -6,7 +6,7 @@ shared="$(dirname "$(readlink -f "$0")")/../shared"
 
 paru -Sy --noconfirm --skipreview --needed \
     imagemagick powershell-bin bun neovim-git openssh gnupg wget jq fuse make grep ripgrep cmake unzip go python3 ruby rustup nodejs clang luarocks tree-sitter-cli \
-    zls vue-language-server taplo-cli sqls pyright vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman roslyn-ls ada_language_server-bin phpactor-bin php85-iconv \
+    zls vue-language-server taplo-cli sqls pyright vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver gopls dockerfile-language-server lua-language-server bash-language-server powershell-editor-services lemminx tailwindcss-language-server yaml-language-server marksman roslyn-ls ada_language_server-bin phpactor-bin \
     netcoredbg delve python-debugpy codelldb-bin vscode-js-debug-bin vscode-bash-debug bashdb \
     ruff eslint_d prettierd yamllint python-cfn-lint markdownlint-cli2
 
