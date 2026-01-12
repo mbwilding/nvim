be4a7e94c02611c15a4730223c9c2a6996eeb9fc
Matthew Wilding <mbwilding@gmail.com>
2026-01-12 20:39:39 +0800

    add tuis

--- lua/plugins/tuis.lua
+++ lua/plugins/tuis.lua
@@ -1,9 +1,13 @@
 return {
     "jrop/tuis.nvim",
-    config = function()
-        -- Optional: set up keymaps
-        vim.keymap.set("n", "<leader>\\", function()
-            require("tuis").choose()
-        end, { desc = "TUIs" })
-    end,
+    lazy = true,
+    keys = {
+        {
+            "<leader>\\",
+            function()
+                require("tuis").choose()
+            end,
+            desc = "TUIs: Choose",
+        },
+    },
 }
