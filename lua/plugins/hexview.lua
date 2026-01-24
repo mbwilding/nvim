78a080dec90a7f1d956c520120ce89cc9cb9e50b
Matthew Wilding <mbwilding@gmail.com>
2026-01-25 02:33:49 +0800

    update

--- lua/plugins/hexview.lua
+++ lua/plugins/hexview.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = {
+        "*.bin",
+    },
+    once = true,
+    callback = function()
+        vim.schedule(function()
+            vim.cmd("Hex")
+        end)
+    end,
+})
+
 return {
     "DamianVCechov/hexview.nvim",
     opts = {},
