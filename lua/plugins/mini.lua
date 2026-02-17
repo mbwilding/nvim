6fba450e2c8fd9c5f0cbe74fb43ff0c1741554f0
Matthew Wilding <mbwilding@gmail.com>
2026-02-17 16:35:53 +0800

    update

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -50,9 +50,9 @@ return {
                 terminal = false,
             },
             mappings = {
-                ["\""] = { action = "closeopen", pair = "\"\"", neigh_pattern = "^[^\\]", register = { cr = false } },
-                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "^[^%a\\]", register = { cr = false } },
-                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "^[^\\]", register = { cr = false } },
+                ["\""] = { action = "open", pair = "\"\"", neigh_pattern = "[^\\\"][^\"]", register = { cr = false } },
+                ["'"] = { action = "open", pair = "''", neigh_pattern = "[^\\'][^']", register = { cr = false } },
+                ["`"] = { action = "open", pair = "``", neigh_pattern = "[^\\`][^`]", register = { cr = false } },
                 ["("] = { action = "open", pair = "()", neigh_pattern = "^[^\\]" },
                 [")"] = { action = "close", pair = "()", neigh_pattern = "^[^\\]" },
                 ["["] = { action = "open", pair = "[]", neigh_pattern = "^[^\\]" },
