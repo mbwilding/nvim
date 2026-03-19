42f9d3a28bc9c71e406c55e6bdafbce2ca15e9fe
Matthew Wilding <mbwilding@gmail.com>
2026-03-19 12:57:19 +0800

    2026-03-19-12-57-19

---
+++ lua/plugins/codereview.lua
@@ -0,0 +1,17 @@
+return {
+    "afewyards/codereview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+        "CodeReview",
+        "CodeReviewAI",
+        "CodeReviewAIFile",
+        "CodeReviewStart",
+        "CodeReviewSubmit",
+        "CodeReviewApprove",
+        "CodeReviewOpen",
+        "CodeReviewPipeline",
+        "CodeReviewComments",
+        "CodeReviewFiles",
+    },
+    opts = {},
+}
