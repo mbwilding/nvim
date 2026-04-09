6e848bd151b794412e96e29ac01b0485c97c2428
Matthew Wilding <mbwilding@gmail.com>
2026-04-09 18:31:37 +0800

    update

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
-require("config")
+local modules = {
+    "settings",
+    "signs",
+    "keymaps",
+    "autocmds",
+    "cmds",
+    "platform",
+    "neovide",
+    "lazy",
+}
+
+for _, module in ipairs(modules) do
+    require("./" .. module)
+end
