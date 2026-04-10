2330c4550082d3e8ba9a20ffb3679002a4b4772c
Matthew Wilding <mbwilding@gmail.com>
2026-04-10 21:43:14 +0800

    2026-04-10-21-43-14

--- init.lua
+++ init.lua
@@ -4,15 +4,15 @@ local function autoload(dir)
     local path = dir and base .. dir or base
     local files = vim.fn.readdir(path)
     for _, name in ipairs(files) do
-        if vim.fn.isdirectory(path .. "/" .. name) == 1 then
-            if not name:match("^_") then
+        if not name:match("^_") then
+            if vim.fn.isdirectory(path .. "/" .. name) == 1 then
                 autoload(dir and (dir .. "/" .. name) or name)
-            end
-        else
-            local mod = name:match("^([^_].+)%.lua$")
-            if mod then
-                local req = dir and (dir:gsub("/", ".") .. "." .. mod) or ("./" .. mod)
-                require(req)
+            else
+                local mod = name:match("^(.+)%.lua$")
+                if mod then
+                    local req = dir and (dir:gsub("/", ".") .. "." .. mod) or ("./" .. mod)
+                    require(req)
+                end
             end
         end
     end
