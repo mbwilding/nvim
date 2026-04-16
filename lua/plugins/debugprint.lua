b5577e9eff0a18c371a96178681f1c2ae3636531
Matthew Wilding <mbwilding@gmail.com>
2026-04-16 13:52:26 +0800

    debugprint

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,7 +1,4 @@
--- Debug print line adding
-
--- g/p or g/P
--- g/v
+-- Debug printing
 
 ---@type zpack.Spec
 return {
@@ -12,9 +9,35 @@ return {
     },
     opts = {
         highlight_lines = false,
-        print_tag = "DEBUG",
+        print_tag = "DEBUGPRINT",
         display_location = false,
         display_counter = true,
         display_snippet = false,
+        keymaps = {
+            normal = {
+                plain_below = "g?p",
+                plain_above = "g?P",
+                variable_below = "g?v",
+                variable_above = "g?V",
+                variable_below_alwaysprompt = false,
+                variable_above_alwaysprompt = false,
+                surround_plain = "g?sp",
+                surround_variable = "g?sv",
+                surround_variable_alwaysprompt = false,
+                textobj_below = "g?o",
+                textobj_above = "g?O",
+                textobj_surround = "g?so",
+                toggle_comment_debug_prints = false,
+                delete_debug_prints = false,
+            },
+            insert = {
+                plain = "<C-G>p",
+                variable = "<C-G>v",
+            },
+            visual = {
+                variable_below = "g?v",
+                variable_above = "g?V",
+            },
+        },
     },
 }
