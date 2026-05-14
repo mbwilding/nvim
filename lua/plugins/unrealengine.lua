201dc4fe150e0856a4eb92a2d49c26b53391eb5c
Matthew Wilding <mbwilding@gmail.com>
2026-05-14 20:10:39 +0800

    Update

--- lua/plugins/unrealengine.lua
+++ lua/plugins/unrealengine.lua
@@ -1,6 +1,6 @@
 local uname = vim.loop.os_uname()
 
-local engine_path = uname.sysname == "Windows_NT" and [[D:\UE\UE_5.7]] or vim.fn.expand('~') .. '/UnrealEngine'
+local engine_path = uname.sysname == "Windows_NT" and [[C:\UE\UE_5.7]] or vim.fn.expand('~') .. '/UnrealEngine'
 
 return {
     "mbwilding/UnrealEngine.nvim",
